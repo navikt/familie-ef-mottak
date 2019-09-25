ea7b313ac66e74841deb3bfaf28d791f7c84faba
Nina Kjekstad <nina.kjekstad@bekk-mac-01818.local>
2019-09-25 09:48:06 +0200

    test logging

--- src/main/kotlin/no/nav/familie/ef/mottak/api/SøknadController.kt
+++ src/main/kotlin/no/nav/familie/ef/mottak/api/SøknadController.kt
@@ -1,14 +1,20 @@
 package no.nav.familie.ef.mottak.api
 
 import no.nav.familie.ef.mottak.api.dto.Kvittering
+import no.nav.familie.ef.mottak.log
 import no.nav.familie.ef.mottak.repository.domain.Henvendelse
 import no.nav.familie.ef.mottak.service.MottakServiceImpl
 import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import org.springframework.web.bind.annotation.*
+import io.micrometer.core.instrument.MeterRegistry
+import io.micrometer.core.instrument.Tags
+import org.slf4j.LoggerFactory
 
 @RestController
 @RequestMapping(path = ["/api/soknad"], produces = [APPLICATION_JSON_VALUE])
-class SøknadController(val mottakServiceImpl: MottakServiceImpl) {
+class SøknadController(private val registry: MeterRegistry, val mottakServiceImpl: MottakServiceImpl) {
+
+    private val log = LoggerFactory.getLogger(SøknadController::class.java)
 
     @PostMapping("sendInn")
     fun sendInn(@RequestBody søknadDto: String): Kvittering {
@@ -17,6 +23,9 @@ class SøknadController(val mottakServiceImpl: MottakServiceImpl) {
 
     @GetMapping("{id}")
     fun get(@PathVariable id: Long): Henvendelse {
+        registry.counter("familie-ef-mottak.testcounter", Tags.of("type", "fatale")).increment()
+        log.info("Test info")
+        log.error("Test error")
         return mottakServiceImpl.get(id)
     }
 
--- src/main/kotlin/no/nav/familie/ef/mottak/config/ApplicationConfig.kt
+++ src/main/kotlin/no/nav/familie/ef/mottak/config/ApplicationConfig.kt
@@ -15,13 +15,10 @@ import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.transaction.PlatformTransactionManager
 import org.springframework.web.client.RestOperations
 import javax.sql.DataSource
-import no.nav.familie.ef.mottak.log
 
 @SpringBootConfiguration
 @EnableScheduling
 class ApplicationConfig : JdbcConfiguration() {
-
-    private val log = log();
 
     @Bean
     fun restTemplate(vararg interceptors: ClientHttpRequestInterceptor): RestOperations =
@@ -29,7 +26,6 @@ class ApplicationConfig : JdbcConfiguration() {
 
     @Bean
     fun dataSource(): DataSource {
-        log.info("Test logging");
         return EmbeddedDatabaseBuilder()
                 .generateUniqueName(true)
                 .setType(EmbeddedDatabaseType.HSQL)
