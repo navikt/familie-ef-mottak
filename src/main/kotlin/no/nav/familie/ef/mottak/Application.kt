ce927530d11e20870a384ec83ca3823571087c3b
ma10s <mattis.janitz@nav.no>
2019-11-11 14:22:24 +0100

    Oppdater til 2.2.1.RELEASE (org.springframework.boot) Legg til @ConfigurationPropertiesScan

--- src/main/kotlin/no/nav/familie/ef/mottak/Application.kt
+++ src/main/kotlin/no/nav/familie/ef/mottak/Application.kt
@@ -2,10 +2,12 @@ package no.nav.familie.ef.mottak
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import springfox.documentation.swagger2.annotations.EnableSwagger2
 
-@SpringBootApplication(scanBasePackages = ["no.nav.familie.ef.mottak"])
+@SpringBootApplication()
 @EnableSwagger2
+@ConfigurationPropertiesScan(basePackages = ["no.nav.familie.ef.mottak"])
 class Application
 
 fun main(args: Array<String>) {
