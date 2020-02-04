03d884bd8cda516661ff5a133cd9a740f60c5e9f
ma10s <mattis.janitz@nav.no>
2020-02-04 14:56:02 +0100

    Oppdater til siste versjon av kontrakt. Gjør kontroller for å hente ut soknad tilgjengelig i test scope.

--- src/test/kotlin/no/nav/familie/ef/mottak/mockapi/SøknadControllerExtension.kt
+++ src/test/kotlin/no/nav/familie/ef/mottak/mockapi/SøknadControllerExtension.kt
@@ -1,11 +1,20 @@
 package no.nav.familie.ef.mottak.no.nav.familie.ef.mottak.mockapi
 
-import no.nav.familie.ef.mottak.api.SøknadController
 import no.nav.familie.ef.mottak.repository.domain.Soknad
+import no.nav.familie.ef.mottak.service.SøknadService
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
-@GetMapping("{id}")
-fun SøknadController.get(@PathVariable id: String): Soknad {
-    return søknadService.get(id)
+
+@RestController
+@RequestMapping(path = ["/testapi/soknad/"], produces = [MediaType.APPLICATION_JSON_VALUE])
+class SøknadControllerExtension(val søknadService: SøknadService) {
+
+    @GetMapping("{id}")
+    fun get(@PathVariable id: String): Soknad {
+        return søknadService.get(id)
+    }
 }
