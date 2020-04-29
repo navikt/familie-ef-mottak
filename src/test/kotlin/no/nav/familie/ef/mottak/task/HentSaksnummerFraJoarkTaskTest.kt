17912a70086156df52be24897c89f35452379fa9
charliemidtlyng <charlie.midtlyng@bekk.no>
2020-04-29 16:09:21 +0200

    Lagt til enkle tester for ruting av neste task

---
+++ src/test/kotlin/no/nav/familie/ef/mottak/task/HentSaksnummerFraJoarkTaskTest.kt
@@ -0,0 +1,29 @@
+package no.nav.familie.ef.mottak.task
+
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.slot
+import no.nav.familie.prosessering.domene.Task
+import no.nav.familie.prosessering.domene.TaskRepository
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import java.util.*
+
+internal class HentSaksnummerFraJoarkTaskTest {
+    val taskRepository: TaskRepository = mockk()
+    val hentSaksnummerFraJoarkTask: HentSaksnummerFraJoarkTask = HentSaksnummerFraJoarkTask(taskRepository, mockk())
+
+    @Test
+    fun `Skal gå til SendSøknadTilSakTask når HentSaksnummerFraJoark er utført`() {
+        val slot = slot<Task>()
+        every {
+            taskRepository.save(capture(slot))
+        } answers {
+            slot.captured
+        }
+
+        hentSaksnummerFraJoarkTask.onCompletion(Task.nyTask(type = "", payload = "", properties = Properties()))
+
+        assertEquals(SendSøknadTilSakTask.SEND_SØKNAD_TIL_SAK, slot.captured.taskStepType)
+    }
+}
