e5b1fdda3eaa4da54bd0cc0b3888571311ffa5ae
Johan Blomgren <johan.blomgren@nav.no>
2020-06-18 21:49:14 +0200

    Prøver med å legge til testcontainer i stedet

--- src/test/resources/db/migration/V4__task_opprettet_søknad.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE soknad ADD task_opprettet BOOLEAN      NOT NULL DEFAULT FALSE;          -- annerledes (splittet opp i separata alter table)
-ALTER TABLE soknad ADD opprettet_tid  TIMESTAMP(3) NOT NULL DEFAULT localtimestamp; -- annerledes
-
-UPDATE soknad
-SET task_opprettet = TRUE
-WHERE EXISTS(SELECT payload
-             FROM task t
-             WHERE t.payload = soknad.id);
