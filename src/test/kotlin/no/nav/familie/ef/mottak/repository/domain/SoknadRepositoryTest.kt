9f4fac78b75eb1e8e128fe4355ff20f14ff4ba0c
Bent J. Lorentzen <bent.lorentzen@nav.no>
2020-06-26 15:01:19 +0200

    Egen tabell og entitet for vedlegg.

--- src/test/kotlin/no/nav/familie/ef/mottak/repository/domain/SoknadRepositoryTest.kt
+++ src/test/kotlin/no/nav/familie/ef/mottak/repository/domain/SoknadRepositoryTest.kt
@@ -3,20 +3,18 @@ package no.nav.familie.ef.mottak.repository.domain
 import no.nav.familie.ef.mottak.IntegrasjonSpringRunnerTest
 import no.nav.familie.ef.mottak.config.DOKUMENTTYPE_OVERGANGSSTØNAD
 import no.nav.familie.ef.mottak.repository.SoknadRepository
-import no.nav.familie.ef.mottak.task.LagPdfTask
-import no.nav.familie.prosessering.domene.Task
-import no.nav.familie.prosessering.domene.TaskRepository
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.test.context.ActiveProfiles
-import java.util.*
 
 @ActiveProfiles("local")
 internal class SoknadRepositoryTest : IntegrasjonSpringRunnerTest() {
 
     @Autowired
     lateinit var soknadRepository: SoknadRepository
+
 
     @Test
     internal fun `findFirstByTaskOpprettetIsFalse returnerer én soknad med taskOpprettet false`() {
@@ -42,9 +40,19 @@ internal class SoknadRepositoryTest : IntegrasjonSpringRunnerTest() {
 
     @Test
     internal fun `findFirstByTaskOpprettetIsFalse takler null result`() {
+        soknadRepository.save(Soknad(søknadJson = "kåre",
+                                     fnr = "ded",
+                                     dokumenttype = DOKUMENTTYPE_OVERGANGSSTØNAD,
+                                     vedlegg = null,
+                                     taskOpprettet = true))
+
         val soknadUtenTask = soknadRepository.findFirstByTaskOpprettetIsFalse()
 
         assertThat(soknadUtenTask).isNull()
     }
 
+    @AfterEach
+    fun tearDown() {
+        soknadRepository.deleteAll()
+    }
 }
