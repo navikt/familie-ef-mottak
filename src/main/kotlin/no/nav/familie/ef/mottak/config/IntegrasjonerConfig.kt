d65c0a39eea4a2d1992166834cc8a5b8fe0b5f25
Bent J. Lorentzen <bent.lorentzen@nav.no>
2020-06-29 16:27:15 +0200

    Endret hentSaksnummer til å ta i mot dto fra integrasjoner og lagt til wiremock-tester.

--- src/main/kotlin/no/nav/familie/ef/mottak/config/IntegrasjonerConfig.kt
+++ src/main/kotlin/no/nav/familie/ef/mottak/config/IntegrasjonerConfig.kt
@@ -2,7 +2,8 @@ package no.nav.familie.ef.mottak.config
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
+import java.net.URI
 
 @ConfigurationProperties("familie.ef.integrasjoner")
 @ConstructorBinding
-data class IntegrasjonerConfig(val url: String)
+data class IntegrasjonerConfig(val url: URI)
