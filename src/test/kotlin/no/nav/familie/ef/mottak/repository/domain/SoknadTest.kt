4035d88cb9bef2dbe6cf1259a8648d453f6593ab
Johan Blomgren <johan.blomgren@nav.no>
2020-06-30 09:22:32 +0200

    Fikset test

--- src/test/kotlin/no/nav/familie/ef/mottak/repository/domain/SoknadTest.kt
+++ src/test/kotlin/no/nav/familie/ef/mottak/repository/domain/SoknadTest.kt
@@ -3,6 +3,7 @@ package no.nav.familie.ef.mottak.repository.domain
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
+import java.time.LocalDateTime
 import java.util.*
 
 internal class SoknadTest {
@@ -10,16 +11,18 @@ internal class SoknadTest {
     @Test
     fun testHashCode() {
         val uuid = UUID.randomUUID().toString()
-        val one = Soknad(uuid, "string", Fil("321".toByteArray()), "", "654", "789", "123", true, "vedlegg")
-        val other = Soknad(uuid, "string", Fil("321".toByteArray()), "", "654", "789", "123", true, "vedlegg")
+        val opprettet = LocalDateTime.now()
+        val one = Soknad(uuid, "string", Fil("321".toByteArray()), "", "654", "789", "123", true, "vedlegg", opprettet)
+        val other = Soknad(uuid, "string", Fil("321".toByteArray()), "", "654", "789", "123", true, "vedlegg", opprettet)
         assertEquals(one.hashCode(), other.hashCode())
     }
 
     @Test
     fun testEquals() {
         val uuid = UUID.randomUUID().toString()
-        val one = Soknad(uuid, "string", Fil("321".toByteArray()), "", "654", "789", "123", true, "vedlegg")
-        val other = Soknad(uuid, "string", Fil("321".toByteArray()), "", "654", "789", "123", true, "vedlegg")
+        val opprettet = LocalDateTime.now()
+        val one = Soknad(uuid, "string", Fil("321".toByteArray()), "", "654", "789", "123", true, "vedlegg", opprettet)
+        val other = Soknad(uuid, "string", Fil("321".toByteArray()), "", "654", "789", "123", true, "vedlegg", opprettet)
         assertTrue(one == other)
     }
 }
