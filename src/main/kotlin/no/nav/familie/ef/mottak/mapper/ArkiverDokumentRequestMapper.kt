ce6aceb1dc9ff8627db7da4ce4c1018082650ea7
Johan Blomgren <johan.blomgren@nav.no>
2020-08-05 12:59:17 +0200

    Fikset dokumenttype for vedlegg

--- src/main/kotlin/no/nav/familie/ef/mottak/mapper/ArkiverDokumentRequestMapper.kt
+++ src/main/kotlin/no/nav/familie/ef/mottak/mapper/ArkiverDokumentRequestMapper.kt
@@ -22,7 +22,21 @@ object ArkiverDokumentRequestMapper {
         return ArkiverDokumentRequest(soknad.fnr,
                                       false,
                                       hoveddokumentvarianter,
-                                      mapVedlegg(vedlegg, mapDokumenttype(soknad.dokumenttype)))
+                                      mapVedlegg(vedlegg, soknad.dokumenttype))
+    }
+
+    private fun mapVedlegg(vedlegg: List<Vedlegg>, dokumenttype: String): List<Dokument> {
+        if(vedlegg.isEmpty()) return emptyList()
+        val dokumenttypeVedlegg = mapDokumenttype(dokumenttype)
+        return vedlegg.map { tilDokument(it, dokumenttypeVedlegg) }
+    }
+
+    private fun tilDokument(vedlegg: Vedlegg, dokumenttypeVedlegg: String): Dokument {
+        return Dokument(dokument = vedlegg.innhold.bytes,
+                        filType = FilType.PDFA,
+                        tittel = vedlegg.tittel,
+                        filnavn = vedlegg.navn,
+                        dokumentType = dokumenttypeVedlegg)
     }
 
     private fun mapDokumenttype(dokumenttype: String): String {
@@ -32,18 +46,5 @@ object ArkiverDokumentRequestMapper {
             else -> error("Ukjent dokumenttype=$dokumenttype for vedlegg")
         }
     }
-
-    private fun mapVedlegg(vedlegg: List<Vedlegg>, dokumenttypeVedlegg: String): List<Dokument> {
-        return vedlegg.map { tilDokument(it, dokumenttypeVedlegg) }
-    }
-
-    private fun tilDokument(vedlegg: Vedlegg, dokumenttypeVedlegg: String): Dokument {
-        return Dokument(dokument = vedlegg.innhold.bytes,
-                        filType = FilType.PDFA,
-                        tittel = vedlegg.tittel,
-                        filnavn = vedlegg.navn,
-                        dokumentType = DOKUMENTTYPE_OVERGANGSSTØNAD_VEDLEGG)
-    }
-
 
 }
