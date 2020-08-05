e269e001cf4d537985b1de3d0158e7f7c98363bb
Johan Blomgren <johan.blomgren@nav.no>
2020-08-05 11:01:29 +0200

    Lagt til noen tester

--- src/test/kotlin/no/nav/familie/ef/mottak/service/SøknadServiceImplTest.kt
+++ src/test/kotlin/no/nav/familie/ef/mottak/service/SøknadServiceImplTest.kt
@@ -1,8 +1,11 @@
 package no.nav.familie.ef.mottak.service
 
 import no.nav.familie.ef.mottak.IntegrasjonSpringRunnerTest
+import no.nav.familie.ef.mottak.repository.SoknadRepository
 import no.nav.familie.ef.mottak.service.Testdata.skjemaForArbeidssøker
+import no.nav.familie.ef.mottak.service.Testdata.søknadBarnetilsyn
 import no.nav.familie.ef.mottak.service.Testdata.søknadOvergangsstønad
+import no.nav.familie.ef.mottak.service.Testdata.vedlegg
 import no.nav.familie.kontrakter.ef.søknad.SøknadMedVedlegg
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
@@ -13,6 +16,7 @@ import org.springframework.test.context.ActiveProfiles
 internal class SøknadServiceImplTest : IntegrasjonSpringRunnerTest() {
 
     @Autowired(required = true) lateinit var søknadService: SøknadService
+    @Autowired(required = true) lateinit var soknadRepository: SoknadRepository
 
     @Test
     internal fun `lagre skjema for arbeidssøker`() {
@@ -24,6 +28,21 @@ internal class SøknadServiceImplTest : IntegrasjonSpringRunnerTest() {
     @Test
     internal fun `lagre skjema for søknad`() {
         val kvittering = søknadService.mottaOvergangsstønad(SøknadMedVedlegg(søknadOvergangsstønad, emptyList()), emptyMap())
+        val søknad = søknadService.get(kvittering.id)
+        assertThat(søknad).isNotNull
+    }
+
+    @Test
+    internal fun `lagre skjema for søknad overgangsstønad med vedlegg`() {
+        val kvittering = søknadService.mottaOvergangsstønad(SøknadMedVedlegg(søknadOvergangsstønad, vedlegg),
+                                                            vedlegg.map { it.id to it.navn.toByteArray() }.toMap())
+        val søknad = søknadService.get(kvittering.id)
+        assertThat(søknad).isNotNull
+    }
+
+    @Test
+    internal fun `lagre skjema for søknad barnetilsyn`() {
+        val kvittering = søknadService.mottaBarnetilsyn(SøknadMedVedlegg(søknadBarnetilsyn, emptyList()), emptyMap())
         val søknad = søknadService.get(kvittering.id)
         assertThat(søknad).isNotNull
     }
