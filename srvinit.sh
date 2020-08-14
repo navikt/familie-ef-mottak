da0faa00543ea623a4f03a83bbf72018da322265
ma10s <mattis.janitz@nav.no>
2020-08-14 17:30:40 +0200

    New path

--- srvinit.sh
+++ srvinit.sh
@@ -4,5 +4,6 @@ DIR=/var/run/secrets/nais.io/srvfamilie-ef-mot/
 if test -d $DIR;
 then
        export CREDENTIAL_USERNAME=familie-ef-mot
-       export CREDENTIAL_PASSWORD=`cat $DIR/password`
+       export CREDENTIAL_PASSWORD=`cat /var/run/secrets/nais.io/srvfamilie-ef-mot/password`
+       echo "- exported CREDENTIAL_PASSWORD for familie-ef-mot "
 fi
