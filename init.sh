f017ef06fef2368d4706cb57f4a1540cae8d6dd3
ma10s <mattis.janitz@nav.no>
2020-08-14 18:22:45 +0200

    NY PATH

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 export CREDENTIAL_USERNAME=familie-ef-mot
-export CREDENTIAL_PASSWORD=$(cat /serviceuser/srvsyfooprest-sbs/username)
+export CREDENTIAL_PASSWORD=$(cat /var/run/secrets/nais.io/srvfamilie-ef-mot/password)
 echo "- exported CREDENTIAL_PASSWORD for familie-ef-mot "
