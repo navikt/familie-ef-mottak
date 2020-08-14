7426ce9762bc396f653c4479da489c0e6c563acd
ma10s <mattis.janitz@nav.no>
2020-08-14 17:04:24 +0200

    Fix script

--- srvinit.sh
+++ srvinit.sh
@@ -2,6 +2,7 @@
 DIR=/var/run/secrets/nais.io/srvfamilie-ef-mot/
 
 if test -d $DIR;
+then
        export CREDENTIAL_USERNAME=familie-ef-mot
        export CREDENTIAL_PASSWORD=`cat $DIR/password`
 fi
