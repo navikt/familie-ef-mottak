e25f1c3208d9e59cf982c070ffc82eb79d0d9c77
Johan Blomgren <johan.blomgren@nav.no>
2020-09-01 15:37:31 +0200

    Fikset test

--- src/test/kotlin/no/nav/familie/ef/mottak/task/SendSøknadTilSakTaskTest.kt
+++ src/test/kotlin/no/nav/familie/ef/mottak/task/SendSøknadTilSakTaskTest.kt
@@ -1,11 +1,10 @@
 package no.nav.familie.ef.mottak.task
 
-import io.mockk.every
+import io.mockk.Called
 import io.mockk.mockk
-import io.mockk.slot
+import io.mockk.verify
 import no.nav.familie.prosessering.domene.Task
 import no.nav.familie.prosessering.domene.TaskRepository
-import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import java.util.*
 
@@ -15,16 +14,8 @@ internal class SendSøknadTilSakTaskTest {
     private val sendSøknadTilSakTask: SendSøknadTilSakTask = SendSøknadTilSakTask(mockk(), taskRepository)
 
     @Test
-    fun `Skal gå til SendMeldingTilDittNavTask når SendSøknadTilSakTask er utført`() {
-        val slot = slot<Task>()
-        every {
-            taskRepository.save(capture(slot))
-        } answers {
-            slot.captured
-        }
-
+    fun `Skal ikke opprette noen ny task`() {
         sendSøknadTilSakTask.onCompletion(Task.nyTask(type = "", payload = "", properties = Properties()))
-
-        assertEquals(SendMeldingTilDittNavTask.SEND_MELDING_TIL_DITT_NAV, slot.captured.taskStepType)
+        verify { listOf(taskRepository) wasNot Called }
     }
 }
