3974f99f7686b9c5d1653961193565af7577dd33
Johan Blomgren <blommish@users.noreply.github.com>
2020-09-11 12:37:36 +0200

    Det skal ikke være mulig å sende inn en søknad 2 ganger med referens til samme vedlegg (#278)

--- src/test/kotlin/no/nav/familie/ef/mottak/api/SøknadControllerTest.kt
+++ src/test/kotlin/no/nav/familie/ef/mottak/api/SøknadControllerTest.kt
@@ -1,9 +1,6 @@
 package no.nav.familie.ef.mottak.api
 
-import io.mockk.mockk
-import io.mockk.verify
 import no.nav.familie.ef.mottak.IntegrasjonSpringRunnerTest
-import no.nav.familie.ef.mottak.service.SøknadService
 import no.nav.familie.ef.mottak.service.Testdata.søknadBarnetilsyn
 import no.nav.familie.ef.mottak.service.Testdata.søknadOvergangsstønad
 import no.nav.familie.ef.mottak.service.Testdata.søknadSkolepenger
@@ -13,27 +10,13 @@ import no.nav.familie.kontrakter.ef.søknad.Vedlegg
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.web.client.exchange
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Primary
-import org.springframework.context.annotation.Profile
 import org.springframework.http.*
 import org.springframework.test.context.ActiveProfiles
+import java.util.*
 
-@Profile("søknad-controller-test")
-@Configuration
-@Primary
-class SøknadControllerTestConfig {
-
-    @Bean fun søknadService(): SøknadService = mockk(relaxed = true)
-}
-
-@ActiveProfiles("local", "søknad-controller-test")
+@ActiveProfiles("local")
 internal class SøknadControllerTest : IntegrasjonSpringRunnerTest() {
-
-    @Autowired lateinit var søknadService: SøknadService
 
     @BeforeEach
     fun setUp() {
@@ -42,48 +25,36 @@ internal class SøknadControllerTest : IntegrasjonSpringRunnerTest() {
 
     @Test
     internal fun `overgangsstønad ok request`() {
-        val søknad = SøknadMedVedlegg(søknadOvergangsstønad, listOf(lagVedlegg("1"), lagVedlegg("2")))
-        okSøknadMedVedleggRequest(søknad, "/api/soknad")
-        okSøknadMedVedleggRequest(søknad, "/api/soknad/overgangsstonad")
-        verify(exactly = 2) { søknadService.mottaOvergangsstønad(søknad, any()) }
+        verifySøknadMedVedleggRequest(SøknadMedVedlegg(søknadOvergangsstønad, listOf(lagVedlegg(), lagVedlegg())), "/api/soknad")
+        verifySøknadMedVedleggRequest(SøknadMedVedlegg(søknadOvergangsstønad, listOf(lagVedlegg(), lagVedlegg())),
+                                      "/api/soknad/overgangsstonad")
     }
 
     @Test
     internal fun `barnetilsyn ok request`() {
-        val søknad = SøknadMedVedlegg(søknadBarnetilsyn, listOf(lagVedlegg("1"), lagVedlegg("2")))
-        okSøknadMedVedleggRequest(søknad, "/api/soknad/barnetilsyn")
-        verify(exactly = 1) { søknadService.mottaBarnetilsyn(søknad, any()) }
+        val søknad = SøknadMedVedlegg(søknadBarnetilsyn, listOf(lagVedlegg(), lagVedlegg()))
+        verifySøknadMedVedleggRequest(søknad, "/api/soknad/barnetilsyn")
     }
 
     @Test
     internal fun `skolepenger ok request`() {
-        val søknad = SøknadMedVedlegg(søknadSkolepenger, listOf(lagVedlegg("1"), lagVedlegg("2")))
-        okSøknadMedVedleggRequest(søknad, "/api/soknad/skolepenger")
-        verify(exactly = 1) { søknadService.mottaSkolepenger(søknad, any()) }
+        val søknad = SøknadMedVedlegg(søknadSkolepenger, listOf(lagVedlegg(), lagVedlegg()))
+        verifySøknadMedVedleggRequest(søknad, "/api/soknad/skolepenger")
     }
 
-    private fun <T> okSøknadMedVedleggRequest(søknad: SøknadMedVedlegg<T>,
-                                              url: String) {
-        headers.set(HttpHeaders.CONTENT_TYPE, MediaType.MULTIPART_FORM_DATA_VALUE)
-        val request = MultipartBuilder()
-                .withJson("søknad", søknad)
-                .withByteArray("vedlegg", "1", byteArrayOf(12))
-                .withByteArray("vedlegg", "2", byteArrayOf(12))
-                .build()
-
-        val response: ResponseEntity<Any> =
-                restTemplate.exchange(localhost(url),
-                                      HttpMethod.POST,
-                                      HttpEntity(request, headers))
-
-        assertThat(response.statusCode).isEqualTo(HttpStatus.OK)
+    @Test
+    internal fun `det skal ikke være mulig å sende inn samme vedlegg på nytt med ny søknad`() {
+        val vedleggId = UUID.randomUUID().toString()
+        val søknad = SøknadMedVedlegg(søknadOvergangsstønad, listOf(lagVedlegg(vedleggId)))
+        verifySøknadMedVedleggRequest(søknad, "/api/soknad/overgangsstonad")
+        verifySøknadMedVedleggRequest(søknad, "/api/soknad/overgangsstonad", HttpStatus.BAD_REQUEST)
     }
 
     @Test
     internal fun `vedlegg savnes i json`() {
         headers.set(HttpHeaders.CONTENT_TYPE, MediaType.MULTIPART_FORM_DATA_VALUE)
         val request = MultipartBuilder()
-                .withJson("søknad", SøknadMedVedlegg(søknadOvergangsstønad, listOf(lagVedlegg("1"))))
+                .withJson("søknad", SøknadMedVedlegg(søknadOvergangsstønad, listOf(lagVedlegg())))
                 .build()
         val response: ResponseEntity<Any> =
                 restTemplate.exchange(localhost("/api/soknad"),
@@ -108,5 +79,24 @@ internal class SøknadControllerTest : IntegrasjonSpringRunnerTest() {
         assertThat(response.statusCode).isEqualTo(HttpStatus.BAD_REQUEST)
     }
 
-    private fun lagVedlegg(id: String) = Vedlegg(id, "navn", "tittel")
+    private fun <T> verifySøknadMedVedleggRequest(søknad: SøknadMedVedlegg<T>,
+                                                  url: String,
+                                                  forventetHttpStatus: HttpStatus = HttpStatus.OK) {
+        headers.set(HttpHeaders.CONTENT_TYPE, MediaType.MULTIPART_FORM_DATA_VALUE)
+        val multipartBuilder = MultipartBuilder()
+                .withJson("søknad", søknad)
+
+        søknad.vedlegg.forEach {
+            multipartBuilder.withByteArray("vedlegg", it.id, byteArrayOf(12))
+        }
+
+        val response: ResponseEntity<Any> =
+                restTemplate.exchange(localhost(url),
+                                      HttpMethod.POST,
+                                      HttpEntity(multipartBuilder.build(), headers))
+
+        assertThat(response.statusCode).isEqualTo(forventetHttpStatus)
+    }
+
+    private fun lagVedlegg(id: String = UUID.randomUUID().toString()) = Vedlegg(id, "navn", "tittel")
 }
