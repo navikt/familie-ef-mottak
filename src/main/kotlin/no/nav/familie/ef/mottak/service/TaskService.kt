15ef5a8c99864ce34a9cbfaeb8d1f9f24d8c6ccd
ma10s <mattis.janitz@nav.no>
2020-09-24 14:48:24 +0200

    Legg til feature-toggle og fjern overflødig save av søknad.

--- src/main/kotlin/no/nav/familie/ef/mottak/service/TaskService.kt
+++ src/main/kotlin/no/nav/familie/ef/mottak/service/TaskService.kt
@@ -1,5 +1,6 @@
 package no.nav.familie.ef.mottak.service
 
+import no.nav.familie.ef.mottak.featuretoggle.FeatureToggleService
 import no.nav.familie.ef.mottak.repository.SoknadRepository
 import no.nav.familie.ef.mottak.repository.domain.Soknad
 import no.nav.familie.ef.mottak.task.LagPdfTask
@@ -12,7 +13,8 @@ import javax.transaction.Transactional
 
 @Service
 class TaskService(private val taskRepository: TaskRepository,
-                  private val soknadRepository: SoknadRepository) {
+                  private val soknadRepository: SoknadRepository,
+                  private val featureToggleService: FeatureToggleService) {
 
     @Transactional
     fun opprettPdfTaskForSoknad(it: Soknad) {
@@ -26,13 +28,15 @@ class TaskService(private val taskRepository: TaskRepository,
     }
 
     @Transactional
-    fun sendSøknadMottattTilDittNavTask(it: Soknad) {
-        val properties =
-                Properties().apply { this["søkersFødselsnummer"] = it.fnr }.apply { this["dokumenttype"] = it.dokumenttype }
-        taskRepository.save(Task.nyTask(SEND_SØKNAD_MOTTATT_TIL_DITT_NAV,
-                                        it.id,
-                                        properties))
-        soknadRepository.save(it.copy(taskOpprettet = true))
+    fun sendSøknadMottattTilDittNavTask(søknad: Soknad) {
+        if (featureToggleService.isEnabled("familie.ef.mottak.task-dittnav")) {
+            val properties =
+                    Properties().apply { this["søkersFødselsnummer"] = søknad.fnr }
+                            .apply { this["dokumenttype"] = søknad.dokumenttype }
+            taskRepository.save(Task.nyTask(SEND_SØKNAD_MOTTATT_TIL_DITT_NAV,
+                                            søknad.id,
+                                            properties))
+        }
     }
 
 }
