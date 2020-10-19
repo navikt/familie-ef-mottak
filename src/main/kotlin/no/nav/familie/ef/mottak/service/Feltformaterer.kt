f0bc5e8d855ba4868f5587b05d57d7a36a3fdaf8
Mattis Janitz <mattis.janitz@nav.no>
2020-10-19 14:46:42 +0200

    Update Feltformaterer.kt
    Bruk / som skilletegn mellom alternativer i pdf

--- src/main/kotlin/no/nav/familie/ef/mottak/service/Feltformaterer.kt
+++ src/main/kotlin/no/nav/familie/ef/mottak/service/Feltformaterer.kt
@@ -75,7 +75,7 @@ object Feltformaterer {
 
     private fun feltMap(label: String, verdi: String, alternativer: List<String>? = null): Map<String, String> {
         return if (alternativer != null) {
-            mapOf("label" to label, "verdi" to verdi, "alternativer" to alternativer.joinToString(", "))
+            mapOf("label" to label, "verdi" to verdi, "alternativer" to alternativer.joinToString("/ "))
         } else {
             mapOf("label" to label, "verdi" to verdi)
         }
