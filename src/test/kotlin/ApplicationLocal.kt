015e9a3ddd2edbd3fbc8ac8126203c4ceebf681f
ma10s <mattis.janitz@nav.no>
2020-10-19 13:40:32 +0200

    Ikke ta bort pdf-mock i ApplicationLocal + ikke endre database konfig i application-local.yml

--- src/test/kotlin/ApplicationLocal.kt
+++ src/test/kotlin/ApplicationLocal.kt
@@ -15,6 +15,7 @@ class ApplicationLocal
 fun main(args: Array<String>) {
     SpringApplicationBuilder(ApplicationLocal::class.java)
             .profiles("local",
-                      "mock-integrasjon")
+                      "mock-integrasjon",
+                      "mock-pdf")
             .run(*args)
 }
