d830af247d723245402e4f669ffe35c7d43aa7e8
ma10s <mattis.janitz@nav.no>
2020-10-19 13:27:43 +0200

    Legg til alternativer på Situasjon i mottak

--- src/test/kotlin/ApplicationLocal.kt
+++ src/test/kotlin/ApplicationLocal.kt
@@ -15,7 +15,6 @@ class ApplicationLocal
 fun main(args: Array<String>) {
     SpringApplicationBuilder(ApplicationLocal::class.java)
             .profiles("local",
-                      "mock-integrasjon",
-                      "mock-pdf")
+                      "mock-integrasjon")
             .run(*args)
 }
--- src/test/kotlin/no/nav/familie/ef/mottak/mockapi/SøknadControllerExtension.kt
+++ src/test/kotlin/no/nav/familie/ef/mottak/mockapi/SøknadControllerExtension.kt
@@ -1,7 +1,9 @@
 package no.nav.familie.ef.mottak.mockapi
 
+import no.nav.familie.ef.mottak.repository.domain.Fil
 import no.nav.familie.ef.mottak.repository.domain.Soknad
 import no.nav.familie.ef.mottak.service.SøknadService
+import no.nav.security.token.support.core.api.Unprotected
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -11,10 +13,16 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping(path = ["/testapi/soknad/"], produces = [MediaType.APPLICATION_JSON_VALUE])
+@Unprotected
 class SøknadControllerExtension(val søknadService: SøknadService) {
 
     @GetMapping("{id}")
     fun get(@PathVariable id: String): Soknad {
         return søknadService.get(id)
+    }
+
+    @GetMapping("{id}/pdf")
+    fun getPdf(@PathVariable id: String): Fil? {
+        return søknadService.get(id).søknadPdf
     }
 }
