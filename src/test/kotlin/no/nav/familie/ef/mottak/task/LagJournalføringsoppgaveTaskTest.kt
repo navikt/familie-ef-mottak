4e0f25effb2fd6f1bd77707ab7ee5ff150dd1aed
charliemidtlyng <charlie.midtlyng@bekk.no>
2021-01-06 09:22:17 +0100

    Skal kun opprette sak for stønadene, ikke arbeidssøkerskjemaet

--- src/test/kotlin/no/nav/familie/ef/mottak/task/LagJournalføringsoppgaveTaskTest.kt
+++ src/test/kotlin/no/nav/familie/ef/mottak/task/LagJournalføringsoppgaveTaskTest.kt
@@ -4,12 +4,19 @@ import io.mockk.every
 import io.mockk.mockk
 import io.mockk.slot
 import io.mockk.verify
+import no.nav.familie.ef.mottak.config.DOKUMENTTYPE_BARNETILSYN
+import no.nav.familie.ef.mottak.config.DOKUMENTTYPE_OVERGANGSSTØNAD
+import no.nav.familie.ef.mottak.config.DOKUMENTTYPE_SKJEMA_ARBEIDSSØKER
+import no.nav.familie.ef.mottak.config.DOKUMENTTYPE_SKOLEPENGER
 import no.nav.familie.ef.mottak.featuretoggle.FeatureToggleService
+import no.nav.familie.ef.mottak.repository.SoknadRepository
+import no.nav.familie.ef.mottak.repository.domain.Soknad
 import no.nav.familie.ef.mottak.service.OppgaveService
 import no.nav.familie.prosessering.domene.Task
 import no.nav.familie.prosessering.domene.TaskRepository
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.springframework.data.repository.findByIdOrNull
 import java.util.*
 
 internal class LagJournalføringsoppgaveTaskTest {
@@ -17,8 +24,9 @@ internal class LagJournalføringsoppgaveTaskTest {
     private val taskRepository: TaskRepository = mockk()
     private val oppgaveService: OppgaveService = mockk(relaxed = true)
     private val featureToggleService: FeatureToggleService = mockk(relaxed = true)
+    private val søknadRepository: SoknadRepository = mockk(relaxed = true)
     private val lagJournalføringsoppgaveTask: LagJournalføringsoppgaveTask =
-            LagJournalføringsoppgaveTask(taskRepository, oppgaveService, featureToggleService)
+            LagJournalføringsoppgaveTask(taskRepository, oppgaveService, søknadRepository, featureToggleService)
 
     @Test
     fun `Skal opprette SendMeldingTilDittNavTask og HentSaksnummerFraJoark når LagJournalføringsoppgaveTask er utført`() {
@@ -30,6 +38,62 @@ internal class LagJournalføringsoppgaveTaskTest {
         }
 
         lagJournalføringsoppgaveTask.onCompletion(Task.nyTask(type = "", payload = "", properties = Properties()))
+
+        assertEquals(HentSaksnummerFraJoarkTask.HENT_SAKSNUMMER_FRA_JOARK, slot.captured[0].taskStepType)
+        assertEquals(SendDokumentasjonsbehovMeldingTilDittNavTask.SEND_MELDING_TIL_DITT_NAV, slot.captured[1].taskStepType)
+    }
+
+    @Test
+    fun `Skal opprette sak hvis det er en ny søknad om overgangsstønad`() {
+        val slot = slot<List<Task>>()
+        val soknad = Soknad(id = UUID.randomUUID().toString(), fnr = "12345678901", søknadJson = "", dokumenttype = DOKUMENTTYPE_OVERGANGSSTØNAD)
+        every { taskRepository.saveAll(capture(slot)) } answers { slot.captured }
+        every { featureToggleService.isEnabled(any()) } returns true
+        every { søknadRepository.findByIdOrNull(any()) } returns soknad
+
+        lagJournalføringsoppgaveTask.onCompletion(Task.nyTask(type = "", payload = soknad.id, properties = Properties()))
+
+        assertEquals(OpprettSakTask.TYPE, slot.captured[0].taskStepType)
+        assertEquals(SendDokumentasjonsbehovMeldingTilDittNavTask.SEND_MELDING_TIL_DITT_NAV, slot.captured[1].taskStepType)
+    }
+
+    @Test
+    fun `Skal opprette sak hvis det er en ny søknad om barnetilsyn`() {
+        val slot = slot<List<Task>>()
+        val soknad = Soknad(id = UUID.randomUUID().toString(), fnr = "12345678901", søknadJson = "", dokumenttype = DOKUMENTTYPE_BARNETILSYN)
+        every { taskRepository.saveAll(capture(slot)) } answers { slot.captured }
+        every { featureToggleService.isEnabled(any()) } returns true
+        every { søknadRepository.findByIdOrNull(any()) } returns soknad
+
+        lagJournalføringsoppgaveTask.onCompletion(Task.nyTask(type = "", payload = soknad.id, properties = Properties()))
+
+        assertEquals(OpprettSakTask.TYPE, slot.captured[0].taskStepType)
+        assertEquals(SendDokumentasjonsbehovMeldingTilDittNavTask.SEND_MELDING_TIL_DITT_NAV, slot.captured[1].taskStepType)
+    }
+
+    @Test
+    fun `Skal opprette sak hvis det er en ny søknad om skolepenger`() {
+        val slot = slot<List<Task>>()
+        val soknad = Soknad(id = UUID.randomUUID().toString(), fnr = "12345678901", søknadJson = "", dokumenttype = DOKUMENTTYPE_SKOLEPENGER)
+        every { taskRepository.saveAll(capture(slot)) } answers { slot.captured }
+        every { featureToggleService.isEnabled(any()) } returns true
+        every { søknadRepository.findByIdOrNull(any()) } returns soknad
+
+        lagJournalføringsoppgaveTask.onCompletion(Task.nyTask(type = "", payload = soknad.id, properties = Properties()))
+
+        assertEquals(OpprettSakTask.TYPE, slot.captured[0].taskStepType)
+        assertEquals(SendDokumentasjonsbehovMeldingTilDittNavTask.SEND_MELDING_TIL_DITT_NAV, slot.captured[1].taskStepType)
+    }
+
+    @Test
+    fun `Skal ikke opprette sak hvis det er en nytt arbeidssøkerskjema`() {
+        val slot = slot<List<Task>>()
+        val soknad = Soknad(id = UUID.randomUUID().toString(), fnr = "12345678901", søknadJson = "", dokumenttype = DOKUMENTTYPE_SKJEMA_ARBEIDSSØKER)
+        every { taskRepository.saveAll(capture(slot)) } answers { slot.captured }
+        every { featureToggleService.isEnabled(any()) } returns true
+        every { søknadRepository.findByIdOrNull(any()) } returns soknad
+
+        lagJournalføringsoppgaveTask.onCompletion(Task.nyTask(type = "", payload = soknad.id, properties = Properties()))
 
         assertEquals(HentSaksnummerFraJoarkTask.HENT_SAKSNUMMER_FRA_JOARK, slot.captured[0].taskStepType)
         assertEquals(SendDokumentasjonsbehovMeldingTilDittNavTask.SEND_MELDING_TIL_DITT_NAV, slot.captured[1].taskStepType)
