57d47dca40528052dd380c05a2278ae2913922f3
Johan Blomgren <blommish@users.noreply.github.com>
2021-02-09 16:08:33 +0100

    Infotrygd - opprett sak. Hvis det feilet på natta så setter vi tasken til OK, men oppretter en ny task som kjører kl 06 på morgonen for å ikke logge feilmeldinger i loggem.

--- src/test/kotlin/no/nav/familie/ef/mottak/task/OpprettSakTaskTest.kt
+++ src/test/kotlin/no/nav/familie/ef/mottak/task/OpprettSakTaskTest.kt
@@ -3,9 +3,11 @@ package no.nav.familie.ef.mottak.no.nav.familie.ef.mottak.task
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.slot
+import io.mockk.verify
 import no.nav.familie.ef.mottak.hendelse.JournalføringHendelseServiceTest
 import no.nav.familie.ef.mottak.repository.SoknadRepository
 import no.nav.familie.ef.mottak.repository.domain.Soknad
+import no.nav.familie.ef.mottak.service.DateTimeService
 import no.nav.familie.ef.mottak.service.SakService
 import no.nav.familie.ef.mottak.task.HentSaksnummerFraJoarkTask
 import no.nav.familie.ef.mottak.task.LagBehandleSakOppgaveTask
@@ -15,9 +17,12 @@ import no.nav.familie.prosessering.domene.Task
 import no.nav.familie.prosessering.domene.TaskRepository
 import no.nav.familie.util.FnrGenerator
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.data.repository.findByIdOrNull
+import java.time.LocalDate
+import java.time.LocalDateTime
 import java.util.*
 
 internal class OpprettSakTaskTest {
@@ -25,7 +30,8 @@ internal class OpprettSakTaskTest {
     private val sakService: SakService = mockk()
     private val soknadRepository: SoknadRepository = mockk()
     private val taskRepository: TaskRepository = mockk()
-    private val opprettSakTask = OpprettSakTask(taskRepository, sakService, soknadRepository)
+    private val dateTimeService: DateTimeService = mockk()
+    private val opprettSakTask = OpprettSakTask(taskRepository, sakService, dateTimeService, soknadRepository)
 
     private val saksnummer = "A01"
     private val soknad = Soknad(søknadJson = "",
@@ -34,51 +40,35 @@ internal class OpprettSakTaskTest {
                                 journalpostId = JournalføringHendelseServiceTest.JOURNALPOST_DIGITALSØKNAD,
                                 fnr = FnrGenerator.generer())
 
-    @Test
-    internal fun `skal opprette sak og persistere saksnummer`() {
-        val soknadSlot = slot<Soknad>()
+    private val soknadSlot = slot<Soknad>()
 
-        every {
-            sakService.opprettSak(any(), any())
-        } returns saksnummer
-
+    @BeforeEach
+    internal fun setUp() {
         every {
             soknadRepository.findByIdOrNull("123")
         } returns soknad
-
+        every { dateTimeService.now() } returns LocalDateTime.of(2020, 1, 1, 12, 0)
         every {
             soknadRepository.save(capture(soknadSlot))
         } returns soknad
+    }
 
+    @Test
+    internal fun `skal opprette sak og persistere saksnummer`() {
+        every { taskRepository.save(any()) } answers { firstArg() }
+        mockOpprettSak()
 
-        opprettSakTask.doTask(Task(type = "", payload = "123", properties = Properties().apply {
-            this[LagBehandleSakOppgaveTask.behandleSakOppgaveIdKey] = "999"
-        }))
+        opprettSakTask.doTask(lagTask())
 
         assertThat(soknadSlot.captured.saksnummer).isEqualTo(saksnummer)
     }
 
-
     @Test
     internal fun `skal ikke oppdatere saksnummer på søknaden dersom sak ikke opprettes`() {
-        val soknadSlot = slot<Soknad>()
+        every { taskRepository.save(any()) } answers { firstArg() }
+        mockOpprettSak(sakOpprettet = false)
 
-        every {
-            sakService.opprettSak(any(), any())
-        } returns null
-
-        every {
-            soknadRepository.findByIdOrNull("123")
-        } returns soknad
-
-        every {
-            soknadRepository.save(capture(soknadSlot))
-        } returns soknad
-
-
-        opprettSakTask.doTask(Task(type = "", payload = "123", properties = Properties().apply {
-            this[LagBehandleSakOppgaveTask.behandleSakOppgaveIdKey] = "999"
-        }))
+        opprettSakTask.doTask(lagTask())
 
         assertThat(soknadSlot.captured.saksnummer).isNull()
     }
@@ -86,9 +76,7 @@ internal class OpprettSakTaskTest {
     @Test
     fun `Skal gå til OppdaterBehandleSakOppgaveTask når sak er opprettet`() {
         val slot = slot<Task>()
-        every {
-            soknadRepository.findByIdOrNull("123")
-        } returns soknad
+        mockOpprettSak()
 
         every {
             taskRepository.save(capture(slot))
@@ -96,17 +84,15 @@ internal class OpprettSakTaskTest {
             slot.captured
         }
 
-        opprettSakTask.onCompletion(Task(type = "", payload = "123", properties = Properties()))
+        opprettSakTask.doTask(lagTask())
 
-        Assertions.assertEquals(OppdaterBehandleSakOppgaveTask.TYPE, slot.captured.type)
+        assertEquals(OppdaterBehandleSakOppgaveTask.TYPE, slot.captured.type)
     }
 
     @Test
     fun `Skal gå til HentSaksnummerFraJoarkTask når sak er ikke er opprettet`() {
         val slot = slot<Task>()
-        every {
-            soknadRepository.findByIdOrNull("123")
-        } returns soknad.copy(saksnummer = null)
+        mockOpprettSak(sakOpprettet = false)
 
         every {
             taskRepository.save(capture(slot))
@@ -114,9 +100,68 @@ internal class OpprettSakTaskTest {
             slot.captured
         }
 
-        opprettSakTask.onCompletion(Task(type = "", payload = "123", properties = Properties()))
+        opprettSakTask.doTask(lagTask())
 
-        Assertions.assertEquals(HentSaksnummerFraJoarkTask.HENT_SAKSNUMMER_FRA_JOARK, slot.captured.type)
+        assertEquals(HentSaksnummerFraJoarkTask.HENT_SAKSNUMMER_FRA_JOARK, slot.captured.type)
+    }
+
+    @Test
+    internal fun `skal opprette ny task hvis opprett task feiler og det er natt`() {
+        val slot = slot<Task>()
+        every { dateTimeService.now() } returns LocalDate.of(2021, 1,1).atTime(22, 0)
+        every {
+            sakService.opprettSak(any(), any())
+        } answers {throw RuntimeException("test-feil")}
+
+        every {
+            taskRepository.save(capture(slot))
+        } answers {
+            slot.captured
+        }
+
+        opprettSakTask.doTask(lagTask())
+
+        verify(exactly = 1) {
+            taskRepository.save(any())
+        }
+
+        val now = dateTimeService.now().toLocalDate()
+        assertThat(slot.captured.id).isEqualTo(0L)
+        assertThat(slot.captured.type).isEqualTo(OpprettSakTask.TYPE)
+        assertThat(slot.captured.triggerTid).isEqualTo(now.plusDays(1).atTime(6, 0))
+    }
+
+    @Test
+    internal fun `skal opprette ny task hvis opprett task feiler og det er natt kl 02 - sett ny task til samme dag`() {
+        val slot = slot<Task>()
+        every { dateTimeService.now() } returns LocalDate.of(2021, 1,1).atTime(2, 0)
+        every { sakService.opprettSak(any(), any()) } answers { throw RuntimeException("Feil") }
+        every {
+            taskRepository.save(capture(slot))
+        } answers {
+            slot.captured
+        }
+
+        opprettSakTask.doTask(lagTask())
+
+        verify(exactly = 1) {
+            taskRepository.save(any())
+        }
+
+        assertThat(slot.captured.type).isEqualTo(OpprettSakTask.TYPE)
+        assertThat(slot.captured.id).isEqualTo(0L)
+        assertThat(slot.captured.triggerTid).isEqualTo(dateTimeService.now().toLocalDate().atTime(6, 0))
+    }
+
+    private fun lagTask() = Task(type = OpprettSakTask.TYPE, payload = "123", properties = Properties().apply {
+        this[LagBehandleSakOppgaveTask.behandleSakOppgaveIdKey] = "999"
+    })
+
+    private fun mockOpprettSak(sakOpprettet: Boolean = true) {
+        val response = if (sakOpprettet) saksnummer else null
+        every {
+            sakService.opprettSak(any(), any())
+        } returns response
     }
 
 }
