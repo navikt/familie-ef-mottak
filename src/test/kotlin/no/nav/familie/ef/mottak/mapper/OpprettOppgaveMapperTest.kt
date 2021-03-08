4799e69f2de5e982016ff2a0703bfa1a9935a0d8
charliemidtlyng <charlie.midtlyng@bekk.no>
2021-03-08 12:18:17 +0100

    Skal sette frist på oppgaver basert på om klokka er før eller etter 12

---
+++ src/test/kotlin/no/nav/familie/ef/mottak/mapper/OpprettOppgaveMapperTest.kt
@@ -0,0 +1,44 @@
+package no.nav.familie.ef.mottak.mapper
+
+import io.mockk.mockk
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+import java.time.LocalDateTime
+
+internal class OpprettOppgaveMapperTest {
+
+    @Test
+    internal fun `skal sette frist til 1 dag for journalføringsoppgave opprettet før kl 12`() {
+        val gjeldendeTid = LocalDateTime.of(2020, 1, 1, 0, 0)
+        val frist = OpprettOppgaveMapper(mockk()).lagFristForOppgave(gjeldendeTid)
+        Assertions.assertThat(frist).isEqualTo(gjeldendeTid.plusDays(1).toLocalDate())
+    }
+
+    @Test
+    internal fun `skal sette frist til 2 dager for journalføringsoppgave opprettet etter kl 12`() {
+        val gjeldendeTid = LocalDateTime.of(2020, 1, 1, 13, 0)
+        val frist = OpprettOppgaveMapper(mockk()).lagFristForOppgave(gjeldendeTid)
+        Assertions.assertThat(frist).isEqualTo(gjeldendeTid.plusDays(2).toLocalDate())
+    }
+
+    @Test
+    internal fun `skal sette frist til 2 dager for journalføringsoppgave opprettet kl 12`() {
+        val gjeldendeTid = LocalDateTime.of(2020, 1, 1, 12, 0)
+        val frist = OpprettOppgaveMapper(mockk()).lagFristForOppgave(gjeldendeTid)
+        Assertions.assertThat(frist).isEqualTo(gjeldendeTid.plusDays(2).toLocalDate())
+    }
+
+    @Test
+    internal fun `skal sette frist til 2 dager for oppgave opprettet før kl 12 med ekstradag`() {
+        val gjeldendeTid = LocalDateTime.of(2020, 1, 1, 11, 0)
+        val frist = OpprettOppgaveMapper(mockk()).lagFristForOppgave(gjeldendeTid, 1)
+        Assertions.assertThat(frist).isEqualTo(gjeldendeTid.plusDays(2).toLocalDate())
+    }
+
+    @Test
+    internal fun `skal sette frist til 3 dager for oppgave opprettet etter kl 12 med ekstradag`() {
+        val gjeldendeTid = LocalDateTime.of(2020, 1, 1, 13, 0)
+        val frist = OpprettOppgaveMapper(mockk()).lagFristForOppgave(gjeldendeTid, 1)
+        Assertions.assertThat(frist).isEqualTo(gjeldendeTid.plusDays(3).toLocalDate())
+    }
+}
