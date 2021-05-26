d41683ef61b294b02fdec90085f973d6767ea145
Johan Blomgren <blommish@users.noreply.github.com>
2021-05-26 16:01:27 +0200

    Endret tekster på BehandlesAvApplikasjon etter diskusjon med Mattis og Mirja

--- src/main/kotlin/no/nav/familie/ef/mottak/mapper/BehandlesAvApplikasjon.kt
+++ src/main/kotlin/no/nav/familie/ef/mottak/mapper/BehandlesAvApplikasjon.kt
@@ -4,9 +4,10 @@ import java.net.URI
 
 enum class BehandlesAvApplikasjon(val applikasjon: String, val beskrivelsePrefix: (URI) -> String) {
     EF_SAK("familie-ef-sak", { uri -> "Må behandles i ny løsning - $uri - " }),
-    EF_SAK_BLANKETT("familie-ef-sak-blankett", { uri -> "Kan behandles i ny løsning - $uri - " }),
     EF_SAK_INFOTRYGD("familie-ef-sak-førstegangsbehandling", { uri -> "Kan behandles i ny løsning - $uri - " }),
-    INFOTRYGD("infotrygd", { "Må behandles i infotrygd - " }),
+    EF_SAK_BLANKETT("familie-ef-sak-blankett", { "" }),
+
+    INFOTRYGD("infotrygd", { "" }),
     UAVKLART("",
              { "Søkeren har behandling i ny løsning, må undersøke evt saker i ny løsning før man velger fagsystem. " });
 }
