d80491cc0de1668052829e1a8ff0b5764218ac03
Johan Blomgren <blommish@users.noreply.github.com>
2021-05-27 13:12:12 +0200

    Endret tilbake til null for behandlesAvApplikasjon i oppgave slik det er i master

--- src/main/kotlin/no/nav/familie/ef/mottak/mapper/BehandlesAvApplikasjon.kt
+++ src/main/kotlin/no/nav/familie/ef/mottak/mapper/BehandlesAvApplikasjon.kt
@@ -2,12 +2,12 @@ package no.nav.familie.ef.mottak.mapper
 
 import java.net.URI
 
-enum class BehandlesAvApplikasjon(val applikasjon: String, val beskrivelsePrefix: (URI) -> String) {
+enum class BehandlesAvApplikasjon(val applikasjon: String?, val beskrivelsePrefix: (URI) -> String) {
     EF_SAK("familie-ef-sak", { uri -> "Må behandles i ny løsning - $uri - " }),
     EF_SAK_INFOTRYGD("familie-ef-sak-førstegangsbehandling", { uri -> "Kan behandles i ny løsning - $uri - " }),
     EF_SAK_BLANKETT("familie-ef-sak-blankett", { "" }),
 
-    INFOTRYGD("infotrygd", { "" }),
-    UAVKLART("",
+    INFOTRYGD(null, { "" }),
+    UAVKLART(null,
              { "Søkeren har behandling i ny løsning, må undersøke evt saker i ny løsning før man velger fagsystem. " });
 }
