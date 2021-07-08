94728fb41604acd8a0dc795cacd8cc11d7c00778
sberbom <sberbom@gmail.com>
2021-07-08 08:37:38 +0200

    oppdatert db migrations

--- src/main/resources/db/migration/V16__ettersending.sql
+++ src/main/resources/db/migration/V16__ettersending.sql
@@ -11,3 +11,17 @@ CREATE TABLE ettersending(
     vedlegg        BYTEA,
     behandle_i_ny_saksbehandling BOOLEAN DEFAULT FALSE
 );
+
+CREATE TABLE ettersending_vedlegg (
+     id       UUID    NOT NULL PRIMARY KEY,
+     ettersending_id VARCHAR NOT NULL REFERENCES ettersending,
+     navn     VARCHAR NOT NULL,
+     tittel   VARCHAR NOT NULL,
+     innhold  BYTEA   NOT NULL
+
+);
+
+CREATE TABLE ettersending_dokumentasjonsbehov (
+     ettersending_id VARCHAR NOT NULL PRIMARY KEY REFERENCES ettersending,
+     data        TEXT    NOT NULL
+);
--- src/main/resources/db/migration/V18__ettersending_dokumentasjonsbehov.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE ettersending_dokumentasjonsbehov (
-     ettersending_id VARCHAR NOT NULL PRIMARY KEY REFERENCES ettersending,
-     data        TEXT    NOT NULL
-);
