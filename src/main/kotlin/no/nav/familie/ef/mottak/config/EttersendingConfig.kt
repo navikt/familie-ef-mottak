49fcdd03ae882f299720ca55503f7efe6c83d4ab
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2022-03-14 15:31:19 +0100

    EttersendingUrl som url type i spring config

--- src/main/kotlin/no/nav/familie/ef/mottak/config/EttersendingConfig.kt
+++ src/main/kotlin/no/nav/familie/ef/mottak/config/EttersendingConfig.kt
@@ -2,8 +2,9 @@ package no.nav.familie.ef.mottak.config
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
+import java.net.URL
 
 
 @ConfigurationProperties("ettersending")
 @ConstructorBinding
-data class EttersendingConfig(val ettersendingUrl: String)
+data class EttersendingConfig(val ettersendingUrl: URL)
