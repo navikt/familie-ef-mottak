e318d139f90a677aa3ee0e25e80f2d1e77e1619f
Johan Blomgren <blommish@users.noreply.github.com>
2022-03-28 14:49:04 +0200

    Legger til spring jdbc i pom og fjerner JPA

---
+++ src/main/kotlin/no/nav/familie/ef/mottak/repository/util/InsertUpdateRepositoryImpl.kt
@@ -0,0 +1,30 @@
+package no.nav.familie.ef.mottak.repository.util
+
+import no.nav.familie.ef.mottak.repository.util.InsertUpdateRepository
+import org.springframework.data.jdbc.core.JdbcAggregateOperations
+import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
+
+@Component
+class InsertUpdateRepositoryImpl<T>(val entityOperations: JdbcAggregateOperations) : InsertUpdateRepository<T> {
+
+    @Transactional
+    override fun insert(t: T): T {
+        return entityOperations.insert(t)
+    }
+
+    @Transactional
+    override fun insertAll(list: List<T>): List<T> {
+        return list.map(this::insert)
+    }
+
+    @Transactional
+    override fun update(t: T): T {
+        return entityOperations.update(t)
+    }
+
+    @Transactional
+    override fun updateAll(list: List<T>): List<T> {
+        return list.map(this::update)
+    }
+}
