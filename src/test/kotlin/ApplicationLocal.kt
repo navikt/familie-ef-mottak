a1dbc4ef2d10fa8ac96b0b48afb9c4be4b3f6997
ma10s <mattis.janitz@nav.no>
2022-06-09 09:28:40 +0200

    Bump token validation - bruk @EnableMockOAuth2Server i tester.

--- src/test/kotlin/ApplicationLocal.kt
+++ src/test/kotlin/ApplicationLocal.kt
@@ -1,15 +1,12 @@
 package no.nav.familie.ef.mottak
 
-import no.nav.security.token.support.test.spring.TokenGeneratorConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration
 import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
-import org.springframework.context.annotation.Import
 
 @SpringBootApplication(exclude = [ErrorMvcAutoConfiguration::class])
 @ConfigurationPropertiesScan
-@Import(TokenGeneratorConfiguration::class)
 class ApplicationLocal
 
 fun main(args: Array<String>) {
