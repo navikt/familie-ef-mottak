a2cc6780d2be24b66315ea3be5eeeb442a46181e
Bent J. Lorentzen <bent.lorentzen@nav.no>
2022-06-17 10:50:39 +0200

    Lagt til litt logging.

--- src/main/kotlin/no/nav/familie/ef/mottak/service/RyddeTaskService.kt
+++ src/main/kotlin/no/nav/familie/ef/mottak/service/RyddeTaskService.kt
@@ -7,6 +7,7 @@ import no.nav.familie.prosessering.domene.Task
 import no.nav.familie.prosessering.internal.TaskService
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import java.util.Properties
 import java.util.UUID
 
 @Service
@@ -14,14 +15,23 @@ import java.util.UUID
 class RyddeTaskService(private val taskService: TaskService) {
 
     fun opprettSøknadsreduksjonTask(søknadId: String) {
-        taskService.save(Task(SøknadsreduksjonTask.TYPE, søknadId))
+        val properties = Properties().apply {
+            setProperty("søknadId", søknadId)
+        }
+        taskService.save(Task(SøknadsreduksjonTask.TYPE, søknadId, properties))
     }
 
     fun opprettSøknadsslettingTask(søknadId: String) {
-        taskService.save(Task(SøknadsslettingTask.TYPE, søknadId))
+        val properties = Properties().apply {
+            setProperty("søknadId", søknadId)
+        }
+        taskService.save(Task(SøknadsslettingTask.TYPE, søknadId, properties))
     }
 
     fun opprettEttersendingsslettingTask(ettersendingId: UUID) {
-        taskService.save(Task(EttersendingsslettingTask.TYPE, ettersendingId.toString()))
+        val properties = Properties().apply {
+            setProperty("ettersendingId", ettersendingId.toString())
+        }
+        taskService.save(Task(EttersendingsslettingTask.TYPE, ettersendingId.toString(), properties))
     }
 }
