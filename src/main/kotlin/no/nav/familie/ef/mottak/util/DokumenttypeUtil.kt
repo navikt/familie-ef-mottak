d84aa88c5a9a3fe95b3e32e3d867378d44f81e3b
Johan Blomgren <blommish@users.noreply.github.com>
2022-06-23 11:02:39 +0200

    Fjerner bruk av deprecated StønadType (#793)

--- src/main/kotlin/no/nav/familie/ef/mottak/util/DokumenttypeUtil.kt
+++ src/main/kotlin/no/nav/familie/ef/mottak/util/DokumenttypeUtil.kt
@@ -3,8 +3,8 @@ package no.nav.familie.ef.mottak.util
 import no.nav.familie.ef.mottak.config.DOKUMENTTYPE_BARNETILSYN
 import no.nav.familie.ef.mottak.config.DOKUMENTTYPE_OVERGANGSSTØNAD
 import no.nav.familie.ef.mottak.config.DOKUMENTTYPE_SKOLEPENGER
-import no.nav.familie.kontrakter.ef.felles.StønadType
 import no.nav.familie.kontrakter.felles.dokarkiv.Dokumenttype
+import no.nav.familie.kontrakter.felles.ef.StønadType
 
 fun dokumenttypeTilStønadType(dokumenttype: String): StønadType? {
     return when (dokumenttype) {
