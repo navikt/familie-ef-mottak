e7aebf62c5ce6e70410062a8dff250f81431cd1d
ma10s <mattis.janitz@nav.no>
2022-06-28 14:33:12 +0200

    Fjern unødvendig kommentar

--- src/main/kotlin/no/nav/familie/ef/mottak/service/OppgaveService.kt
+++ src/main/kotlin/no/nav/familie/ef/mottak/service/OppgaveService.kt
@@ -131,7 +131,6 @@ class OppgaveService(
                             finnBehandlendeEnhet(journalpost)
                         )
                     return opprettOppgave(opprettOppgave, journalpost)
-                    // Legg i mappe
                 }
             }
         } else {
