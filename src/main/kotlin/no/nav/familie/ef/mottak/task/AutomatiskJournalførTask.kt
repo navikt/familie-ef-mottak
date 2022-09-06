90173a3f62a705f738d75aa324d312532f74196b
Mattis Janitz <mattis.janitz@nav.no>
2022-09-06 08:28:41 +0200

    Automatisk journalføring taskimpl (#854)
    * Manuell journalføring - vil kalle ef-sak for å opprette førstegangsbehandling og behandle-sak oppgave.
    Bruk riktig urler i SaksbehandlingClient.
    
    * Catch feil i service. Hvis automatisk journalføring ikke gikk bra bruk "fallback" manuel flyt.
    Flytt logging i til service.
    
    * Test av automatisk journalføring service
    
    * Vil bruke journalførAutomatisk, ikke lagFørstegangsbehandlingOgBehandleSakOppgave som i bestefall er upresist. Fjern unødvendige @Transactional.
    
    * Bedre logging

--- src/main/kotlin/no/nav/familie/ef/mottak/task/AutomatiskJournalførTask.kt
+++ src/main/kotlin/no/nav/familie/ef/mottak/task/AutomatiskJournalførTask.kt
@@ -1,16 +1,48 @@
 package no.nav.familie.ef.mottak.task
 
+import no.nav.familie.ef.mottak.repository.domain.Søknad
+import no.nav.familie.ef.mottak.service.AutomatiskJournalføringService
+import no.nav.familie.ef.mottak.service.SøknadService
+import no.nav.familie.ef.mottak.util.dokumenttypeTilStønadType
+import no.nav.familie.kontrakter.felles.ef.StønadType
 import no.nav.familie.prosessering.AsyncTaskStep
 import no.nav.familie.prosessering.TaskStepBeskrivelse
 import no.nav.familie.prosessering.domene.Task
+import no.nav.familie.prosessering.domene.TaskRepository
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
 @TaskStepBeskrivelse(taskStepType = AutomatiskJournalførTask.TYPE, beskrivelse = "Automatisk journalfør")
-class AutomatiskJournalførTask : AsyncTaskStep {
+class AutomatiskJournalførTask(
+    val søknadService: SøknadService,
+    val taskRepository: TaskRepository,
+    val automatiskJournalføringService: AutomatiskJournalføringService
+) :
+    AsyncTaskStep {
+
+    val logger: Logger = LoggerFactory.getLogger(this::class.java)
 
     override fun doTask(task: Task) {
-        TODO("Not yet implemented")
+        val søknad: Søknad = søknadService.get(task.payload)
+        val stønadstype: StønadType =
+            dokumenttypeTilStønadType(søknad.dokumenttype) ?: error("Må ha stønadstype for å automatisk journalføre")
+        val journalpostId = task.metadata["journalpostId"].toString()
+
+        val automatiskJournalføringFullført =
+            automatiskJournalføringService.journalførAutomatisk(
+                personIdent = søknad.fnr,
+                journalpostId = journalpostId,
+                stønadstype = stønadstype
+            )
+
+        if (!automatiskJournalføringFullført) {
+            logger.warn("Kunne ikke automatisk journalføre $journalpostId - fortsetter derfor på manuell journalføringsflyt")
+            val nesteFallbackTaskType = manuellJournalføringFlyt().first().type
+            val fallback = Task(nesteFallbackTaskType, task.payload, task.metadata)
+            taskRepository.save(fallback)
+        }
     }
 
     companion object {
---
+++ src/test/kotlin/no/nav/familie/ef/mottak/service/AutomatiskJournalføringServiceTest.kt
@@ -0,0 +1,60 @@
+package no.nav.familie.ef.mottak.service
+
+import io.mockk.every
+import io.mockk.mockk
+import no.nav.familie.ef.mottak.integration.SaksbehandlingClient
+import no.nav.familie.kontrakter.ef.journalføring.AutomatiskJournalføringResponse
+import no.nav.familie.kontrakter.felles.ef.StønadType
+import org.junit.jupiter.api.Test
+import java.util.UUID
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+internal class AutomatiskJournalføringServiceTest {
+
+    private val saksbehandlingClient = mockk<SaksbehandlingClient>()
+    private val automatiskJournalføringService =
+        AutomatiskJournalføringService(
+            taskRepository = mockk(),
+            søknadService = mockk(),
+            saksbehandlingClient = saksbehandlingClient
+        )
+
+    private val automatiskJournalføringResponse = AutomatiskJournalføringResponse(
+        fagsakId = UUID.randomUUID(),
+        behandlingId = UUID.randomUUID(),
+        behandleSakOppgaveId = 0
+    )
+
+    @Test
+    internal fun `Skal returnere true når journalføring i ef-sak går bra `() {
+
+        every {
+            saksbehandlingClient.journalførAutomatisk(any())
+        } returns automatiskJournalføringResponse
+
+        assertTrue {
+            automatiskJournalføringService.journalførAutomatisk(
+                personIdent = "",
+                journalpostId = "",
+                stønadstype = StønadType.OVERGANGSSTØNAD
+            )
+        }
+    }
+
+    @Test
+    internal fun `Skal returnere false når journalføring i ef-sak går bra `() {
+
+        every {
+            saksbehandlingClient.journalførAutomatisk(any())
+        } throws RuntimeException("Feil")
+
+        assertFalse {
+            automatiskJournalføringService.journalførAutomatisk(
+                personIdent = "",
+                journalpostId = "",
+                stønadstype = StønadType.OVERGANGSSTØNAD
+            )
+        }
+    }
+}
