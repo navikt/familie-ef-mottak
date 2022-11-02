8f7953e30b497eb06ccbedf4d53580fb9d5a1361
Johan Blomgren <blommish@users.noreply.github.com>
2022-11-02 19:55:31 +0100

    Skal lytte på hendelser fra q1-topic for journalposthendelser

--- src/main/kotlin/no/nav/familie/ef/mottak/hendelse/JournalhendelseKafkaListener.kt
+++ src/main/kotlin/no/nav/familie/ef/mottak/hendelse/JournalhendelseKafkaListener.kt
@@ -2,16 +2,13 @@ package no.nav.familie.ef.mottak.hendelse
 
 import no.nav.joarkjournalfoeringhendelser.JournalfoeringHendelseRecord
 import org.apache.kafka.clients.consumer.ConsumerRecord
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.kafka.listener.ConsumerSeekAware
 import org.springframework.kafka.support.Acknowledgment
 import org.springframework.stereotype.Service
 
 @Service
-class JournalhendelseKafkaListener(val kafkaHåndterer: JournalhendelseKafkaHåndterer) {
-
-    val secureLogger: Logger = LoggerFactory.getLogger("secureLogger")
+class JournalhendelseKafkaListener(val kafkaHåndterer: JournalhendelseKafkaHåndterer): ConsumerSeekAware {
 
     @KafkaListener(
         id = "familie-ef-mottak",
@@ -22,5 +19,17 @@ class JournalhendelseKafkaListener(val kafkaHåndterer: JournalhendelseKafkaHån
     )
     fun listen(consumerRecord: ConsumerRecord<String, JournalfoeringHendelseRecord>, ack: Acknowledgment) {
         kafkaHåndterer.håndterHendelse(consumerRecord, ack)
+    }
+
+    override fun onPartitionsAssigned(
+        assignments: MutableMap<org.apache.kafka.common.TopicPartition, Long>,
+        callback: ConsumerSeekAware.ConsumerSeekCallback
+    ) {
+        assignments.keys.stream()
+            .filter { it.topic() == "teamdokumenthandtering.aapen-dok-journalfoering-q1" }
+            .forEach {
+                callback.seekToEnd("teamdokumenthandtering.aapen-dok-journalfoering-q1", it.partition())
+                // callback.seekToBeginning("aapen-person-pdl-leesah-v1", it.partition())
+            }
     }
 }
