10d3f27b5003eea017cfffa78dadde26575df9d3
ma10s <mattis.janitz@nav.no>
2023-01-30 15:29:58 +0100

    Fjerner logging

--- src/main/kotlin/no/nav/familie/ef/mottak/api/SkjemaController.kt
+++ src/main/kotlin/no/nav/familie/ef/mottak/api/SkjemaController.kt
@@ -1,12 +1,10 @@
 package no.nav.familie.ef.mottak.api
 
 import no.nav.familie.ef.mottak.api.dto.Kvittering
-import no.nav.familie.ef.mottak.featuretoggle.FeatureToggleService
 import no.nav.familie.ef.mottak.service.SøknadService
 import no.nav.familie.kontrakter.ef.søknad.SkjemaForArbeidssøker
 import no.nav.familie.sikkerhet.EksternBrukerUtils
 import no.nav.security.token.support.core.api.ProtectedWithClaims
-import org.slf4j.LoggerFactory
 import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -19,13 +17,10 @@ import org.springframework.web.bind.annotation.RestController
     issuer = EksternBrukerUtils.ISSUER_TOKENX,
     claimMap = ["acr=Level4"],
 )
-class SkjemaController(val søknadService: SøknadService, val featureToggleService: FeatureToggleService) {
-    private val logger = LoggerFactory.getLogger(this::class.java)
+class SkjemaController(val søknadService: SøknadService) {
 
     @PostMapping
     fun sendInn(@RequestBody skjemaForArbeidssøker: SkjemaForArbeidssøker): Kvittering {
-        val toggle = featureToggleService.isEnabled("familie.ef.mottak.togglePing")
-        logger.info("familie.ef.mottak.togglePing = $toggle")
         return søknadService.motta(skjemaForArbeidssøker)
     }
 }
