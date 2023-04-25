8d328091af1ee75906393b16e384f27da9b99118
Mattis Janitz <mattis.janitz@nav.no>
2023-04-25 09:57:14 +0200

    Spring boot 3 (#960)
    * kjørt ktlint
    
    * * Oppdatert til spring boot til 3.0.5. Derfor:
    * oppdatert relaterte dependencies eg. felles, kontrakter, prosessering, tokenvalidation mm.
    * Renamet duplikate Beans (samme funksjonsnavn ikke lov - restTemplate brukt flere ganger)
    * Slettet @ConstructorBinding
    * config: ignore-migration-patterns: "*:missing" (Vi mangler noen skript i kodebase etter ny baseline, ignore-missing-migrations: true virker ikke lenger)
    * .pathSegment -> legger til / i start av segment. Vi trenger ikke to, så sletter en.
    * javax -> jakarta
    * Må encode uri på riktig måte: Legger inn pathsegment enkeltvis for ikke encode "/" to ganger i pathSegment (%2) + legger til .encode() for å encode queryParam som kan inneholde Ø.
    
    * * Oppdatert til spring boot til 3.0.5. Derfor:
    * oppdatert relaterte dependencies eg. felles, kontrakter, prosessering, tokenvalidation mm.
    * Renamet duplikate Beans (samme funksjonsnavn ikke lov - restTemplate brukt flere ganger)
    * Slettet @ConstructorBinding
    * config: ignore-migration-patterns: "*:missing" (Vi mangler noen skript i kodebase etter ny baseline, ignore-missing-migrations: true virker ikke lenger)
    * .pathSegment -> legger til / i start av segment. Vi trenger ikke to, så sletter en.
    * javax -> jakarta
    * Må encode uri på riktig måte: Legger inn pathsegment enkeltvis for ikke encode "/" to ganger i pathSegment (%2) + legger til .encode() for å encode queryParam som kan inneholde Ø.
    
    * Formatering.
    
    * Update pom.xml
    
    Co-authored-by: Johan Blomgren <blommish@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Johan Blomgren <blommish@users.noreply.github.com>

--- src/main/kotlin/no/nav/familie/ef/mottak/config/PdfgeneratorConfig.kt
+++ src/main/kotlin/no/nav/familie/ef/mottak/config/PdfgeneratorConfig.kt
@@ -1,8 +1,6 @@
 package no.nav.familie.ef.mottak.config
 
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.boot.context.properties.ConstructorBinding
 
 @ConfigurationProperties("familie.ef.pdfgenerator")
-@ConstructorBinding
 data class PdfgeneratorConfig(val url: String)
