179065eccfe98efc24191ebd2393679ed6c76372
ma10s <mattis.janitz@nav.no>
2023-06-27 09:55:01 +0200

    Vil legge til micrometer teller på varslinger sendt til bruker - dokumentasjonsbehov. Vil telle hvorfor vi ikke sender varsel (bruker- eller saksbehandler askjon).
    Vil også logge dersom både saksbehandler og bruker har gjort noe som stopper varsel.

--- src/main/kotlin/no/nav/familie/ef/mottak/task/SendPåminnelseOmDokumentasjonsbehovTilDittNavTask.kt
+++ src/main/kotlin/no/nav/familie/ef/mottak/task/SendPåminnelseOmDokumentasjonsbehovTilDittNavTask.kt
@@ -1,5 +1,6 @@
 package no.nav.familie.ef.mottak.task
 
+import io.micrometer.core.instrument.Metrics.counter
 import no.nav.brukernotifikasjon.schemas.builders.domain.PreferertKanal
 import no.nav.familie.ef.mottak.config.EttersendingConfig
 import no.nav.familie.ef.mottak.integration.SaksbehandlingClient
@@ -23,11 +24,9 @@ import no.nav.familie.prosessering.domene.Task
 import no.nav.familie.util.VirkedagerProvider
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
-import io.micrometer.core.instrument.Counter
 import java.time.LocalDate
 import java.util.Properties
 import java.util.UUID
-import io.micrometer.core.instrument.Metrics.counter
 
 @Service
 @TaskStepBeskrivelse(
@@ -44,12 +43,13 @@ class SendPåminnelseOmDokumentasjonsbehovTilDittNavTask(
 
     private val logger = LoggerFactory.getLogger(this::class.java)
 
-    val dokumentasjonsbehovvarslinger: Counter = counter("alene.med.barn.dokumentasjonsbehovvarsling.sendt")
-    val dokumentasjonsbehovvarslingerBrukerSendtInn: Counter =
+    val dokumentasjonsbehovvarslinger = counter("alene.med.barn.dokumentasjonsbehovvarsling.sendt")
+    val dokumentasjonsbehovvarslingerBrukerSendtInn =
         counter("alene.med.barn.dokumentasjonsbehovvarslingstoppet.bruker")
-    val dokumentasjonsbehovvarslingerSaksbehandlerStartet: Counter =
+    val dokumentasjonsbehovvarslingerSaksbehandlerStartet =
         counter("alene.med.barn.dokumentasjonsbehovvarslingstoppet.saksbehandler")
-
+    val dokumentasjonsbehovvarslingerSaksbehandlerOgBruker =
+        counter("alene.med.barn.dokumentasjonsbehovvarslingstoppet.saksbehandlerogbruker")
 
     override fun doTask(task: Task) {
         val søknad = søknadService.get(task.payload)
@@ -81,33 +81,54 @@ class SendPåminnelseOmDokumentasjonsbehovTilDittNavTask(
         søknader: List<Søknad>,
         ettersendinger: List<Ettersending>,
         stønadType: StønadType?,
-    ) =
-        harSendtInnNySøknad(søknad, søknader) ||
-                harSendtInnEttersendingIEttertid(søknad, ettersendinger) ||
-                stønadType == null ||
-                tilhørendeBehandleSakOppgaveErPåbegynt(søknad, stønadType)
+    ): Boolean {
+        if (stønadType == null) {
+            return true
+        }
+
+        val brukerHarSendtInnNoe =
+            harSendtInnNySøknad(søknad, søknader) || harSendtInnEttersendingIEttertid(søknad, ettersendinger)
+        val tilhørendeBehandleSakOppgaveErPåbegynt = tilhørendeBehandleSakOppgaveErPåbegynt(søknad, stønadType)
+
+        loggMicrometer(brukerHarSendtInnNoe, tilhørendeBehandleSakOppgaveErPåbegynt)
+
+        return brukerHarSendtInnNoe ||
+            tilhørendeBehandleSakOppgaveErPåbegynt
+    }
+
+    private fun loggMicrometer(
+        brukerHarSendtInnNoe: Boolean,
+        saksbehandlerHarStartet: Boolean,
+    ) {
+        if (brukerHarSendtInnNoe and !saksbehandlerHarStartet) {
+            dokumentasjonsbehovvarslingerBrukerSendtInn.increment()
+        }
+
+        if (saksbehandlerHarStartet and !brukerHarSendtInnNoe) {
+            dokumentasjonsbehovvarslingerSaksbehandlerStartet.increment()
+        }
+
+        if (brukerHarSendtInnNoe and saksbehandlerHarStartet) {
+            dokumentasjonsbehovvarslingerSaksbehandlerOgBruker.increment()
+        }
+    }
 
     private fun harSendtInnNySøknad(søknad: Søknad, søknader: List<Søknad>): Boolean =
         søknader.filter { it.id != søknad.id }
             .filter { SøknadType.hentSøknadTypeForDokumenttype(it.dokumenttype) != SøknadType.OVERGANGSSTØNAD_ARBEIDSSØKER }
             .any { it.opprettetTid > søknad.opprettetTid }
 
-    private fun harSendtInnEttersendingIEttertid(søknad: Søknad, ettersendinger: List<Ettersending>): Boolean {
-        dokumentasjonsbehovvarslingerBrukerSendtInn.increment()
-        return ettersendinger.any { it.opprettetTid > søknad.opprettetTid }
-    }
+    private fun harSendtInnEttersendingIEttertid(søknad: Søknad, ettersendinger: List<Ettersending>): Boolean =
+        ettersendinger.any { it.opprettetTid > søknad.opprettetTid }
 
-    private fun tilhørendeBehandleSakOppgaveErPåbegynt(søknad: Søknad, stønadType: StønadType): Boolean {
-        dokumentasjonsbehovvarslingerSaksbehandlerStartet.increment()
-        return saksbehandlingClient.kanSendePåminnelseTilBruker(
+    private fun tilhørendeBehandleSakOppgaveErPåbegynt(søknad: Søknad, stønadType: StønadType): Boolean =
+        saksbehandlingClient.kanSendePåminnelseTilBruker(
             KanSendePåminnelseRequest(
                 personIdent = søknad.fnr,
                 stønadType = stønadType,
                 innsendtSøknadTidspunkt = søknad.opprettetTid,
-            )
+            ),
         )
-    }
-
 
     private fun lagLinkMelding(søknad: Søknad): LinkMelding {
         val søknadType = SøknadType.hentSøknadTypeForDokumenttype(søknad.dokumenttype)
