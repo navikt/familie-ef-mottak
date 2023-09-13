612a2d9e7881ce246d20e7bf2f02d2557af6737c
throndi <56258085+throndi@users.noreply.github.com>
2023-09-13 18:24:54 +0200

    rettelse

--- src/main/kotlin/no/nav/familie/ef/mottak/featuretoggle/FeatureToggleService.kt
+++ src/main/kotlin/no/nav/familie/ef/mottak/featuretoggle/FeatureToggleService.kt
@@ -1,15 +1,12 @@
 package no.nav.familie.ef.mottak.featuretoggle
 
+import no.nav.familie.unleash.DefaultUnleashService
 import org.springframework.stereotype.Service
 
 @Service
-class FeatureToggleService {
+class FeatureToggleService(val defaultUnleashService: DefaultUnleashService) {
 
     fun isEnabled(toggleId: String): Boolean {
-        return isEnabled(toggleId, false)
-    }
-
-    fun isEnabled(toggleId: String, defaultValue: Boolean): Boolean {
-        return isEnabled(toggleId, defaultValue)
+        return defaultUnleashService.isEnabled(toggleId, false)
     }
 }
