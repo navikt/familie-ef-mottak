e80f6319bdd53b13dbdde3489895a04e388f8795
ma10s <mattis.janitz@nav.no>
2023-09-14 11:05:34 +0200

    Vil lage forvaltningsendepunkt for å oppdatere "ArkiverEttersendingTask" med duplikat callId med ny callId. Dette for å kunne arkivere journalpost uten å få HttpClientErrorException.Conflict.

--- src/main/kotlin/no/nav/familie/ef/mottak/api/ForvaltningController.kt
+++ src/main/kotlin/no/nav/familie/ef/mottak/api/ForvaltningController.kt
@@ -1,7 +1,13 @@
 package no.nav.familie.ef.mottak.api
 
 import no.nav.familie.ef.mottak.service.EttersendingService
+import no.nav.familie.ef.mottak.task.ArkiverEttersendingTask
+import no.nav.familie.log.IdUtils
+import no.nav.familie.log.mdc.MDCConstants
+import no.nav.familie.prosessering.domene.Status
+import no.nav.familie.prosessering.internal.TaskService
 import no.nav.security.token.support.core.api.ProtectedWithClaims
+import org.slf4j.MDC
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -12,14 +18,34 @@ import java.util.UUID
 @RestController
 @RequestMapping(path = ["/api/forvaltning"])
 @ProtectedWithClaims(issuer = "azuread")
-class ForvaltningController(private val ettersendingService: EttersendingService) {
+class ForvaltningController(private val ettersendingService: EttersendingService, private val taskService: TaskService,) {
 
     @PostMapping("/ettersending/splitt")
     fun trekkUtVedleggFraEttersending(@RequestBody ettersendingVedleggId: EttersendingVedleggId): ResponseEntity<String> {
         val nyEttersendingId = ettersendingService.trekkUtEttersendingTilEgenTaskForVedlegg(ettersendingVedleggId.id)
         return ResponseEntity.ok("Opprettet ny ettersending med id: $nyEttersendingId")
     }
+
+    @PostMapping("/ettersending/nycallid")
+    fun nyCallIdPåEttersendingTask(@RequestBody taskId: TaskId): ResponseEntity<String> {
+        val task = taskService.findById(taskId.id)
+        require(task.type == ArkiverEttersendingTask.TYPE)
+        require(task.status == Status.FEILET)
+        val generateId = IdUtils.generateId()
+        task.metadata.apply {
+            this["callId"] = generateId
+        }
+        MDC.put(MDCConstants.MDC_CALL_ID, generateId)
+        taskService.save(task)
+        return ResponseEntity.ok("Endret callId på task til: $generateId")
+    }
+
 }
+
+
+data class TaskId(
+    val id: Long,
+)
 
 data class EttersendingVedleggId(
     val id: UUID,
