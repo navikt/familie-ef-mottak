d145acee59f2c5b6af5e625bc2a05fc34d0eb109
throndi <56258085+throndi@users.noreply.github.com>
2023-09-29 13:51:37 +0200

    Fjerner DefaultUnleashService bean fra FeatureToggleConfig. Endrer navn fra FeatureToggleConfig til CustomUnleashStrategies. Bruker UnleashService fra felles i FeatureToggleService.

---
+++ src/main/kotlin/no/nav/familie/ef/mottak/config/CustomUnleashStrategies.kt
@@ -0,0 +1,15 @@
+package no.nav.familie.ef.mottak.config
+
+import io.getunleash.strategy.Strategy
+import no.nav.familie.ef.mottak.featuretoggle.ByEnvironmentStrategy
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class CustomUnleashStrategies {
+
+    @Bean
+    fun strategies(): List<Strategy> {
+        return listOf(ByEnvironmentStrategy())
+    }
+}
