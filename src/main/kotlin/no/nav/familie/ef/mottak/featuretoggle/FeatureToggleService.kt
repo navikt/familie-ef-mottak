b135d74dd5e57c6d884d7b644f4b8c6ee75a0fe3
Trondl <56258085+throndi@users.noreply.github.com>
2023-10-02 10:23:36 +0200

    Rettelse i konfigurasjon av featuretoggles (#1049)
    * Fjerner DefaultUnleashService bean fra FeatureToggleConfig. Endrer navn fra FeatureToggleConfig til CustomUnleashStrategies. Bruker UnleashService fra felles i FeatureToggleService.
    
    * Setter unleash enabled lik false i application-local

--- src/main/kotlin/no/nav/familie/ef/mottak/featuretoggle/FeatureToggleService.kt
+++ src/main/kotlin/no/nav/familie/ef/mottak/featuretoggle/FeatureToggleService.kt
@@ -1,12 +1,12 @@
 package no.nav.familie.ef.mottak.featuretoggle
 
-import no.nav.familie.unleash.DefaultUnleashService
+import no.nav.familie.unleash.UnleashService
 import org.springframework.stereotype.Service
 
 @Service
-class FeatureToggleService(val defaultUnleashService: DefaultUnleashService) {
+class FeatureToggleService(val unleashService: UnleashService) {
 
     fun isEnabled(toggleId: String): Boolean {
-        return defaultUnleashService.isEnabled(toggleId, false)
+        return unleashService.isEnabled(toggleId, false)
     }
 }
