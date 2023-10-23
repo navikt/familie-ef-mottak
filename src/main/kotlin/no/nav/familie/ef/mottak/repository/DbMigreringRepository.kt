de868c449bec2de61c84b8f5ba8c0307aa761109
ma10s <mattis.janitz@nav.no>
2023-10-23 08:42:53 +0200

    Vil slette kode som ikke er i bruk

--- src/main/kotlin/no/nav/familie/ef/mottak/repository/DbMigreringRepository.kt
+++
@@ -1,8 +0,0 @@
-package no.nav.familie.ef.mottak.repository
-
-import no.nav.familie.ef.mottak.repository.util.RepositoryInterface
-import no.nav.familie.prosessering.domene.Task
-import org.springframework.stereotype.Repository
-
-@Repository
-interface DbMigreringRepository : RepositoryInterface<Task, Long>
