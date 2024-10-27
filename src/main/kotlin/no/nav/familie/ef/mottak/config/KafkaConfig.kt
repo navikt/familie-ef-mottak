78de2c0b75e76c7b81db622b3ccf8859e5f07c48
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2024-10-27 13:19:32 +0100

    Fikset deprecated kode i kafka config. Setter all config i kafkalistenerfactory og fjerner alt i application.yml for å ha all kafka config på ett sted

--- src/main/kotlin/no/nav/familie/ef/mottak/config/KafkaConfig.kt
+++ src/main/kotlin/no/nav/familie/ef/mottak/config/KafkaConfig.kt
@@ -1,8 +1,12 @@
 package no.nav.familie.ef.mottak.config
 
+import io.confluent.kafka.serializers.KafkaAvroDeserializerConfig
 import no.nav.familie.kafka.KafkaErrorHandler
 import no.nav.joarkjournalfoeringhendelser.JournalfoeringHendelseRecord
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.springframework.beans.factory.ObjectProvider
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties
+import org.springframework.boot.ssl.SslBundles
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.annotation.EnableKafka
@@ -18,12 +22,18 @@ class KafkaConfig {
     fun kafkaJournalføringHendelseListenerContainerFactory(
         properties: KafkaProperties,
         kafkaErrorHandler: KafkaErrorHandler,
+        sslBundles: ObjectProvider<SslBundles>,
     ): ConcurrentKafkaListenerContainerFactory<Long, JournalfoeringHendelseRecord> {
-        properties.properties["specific.avro.reader"] = "true"
+        val consumerProperties = properties.buildConsumerProperties(sslBundles.getIfAvailable())
+        consumerProperties[ConsumerConfig.MAX_POLL_RECORDS_CONFIG] = 1
+        consumerProperties[ConsumerConfig.AUTO_OFFSET_RESET_CONFIG] = "earliest"
+        consumerProperties[ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG] = false
+        consumerProperties[KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG] = true
+
         val factory = ConcurrentKafkaListenerContainerFactory<Long, JournalfoeringHendelseRecord>()
         factory.containerProperties.ackMode = ContainerProperties.AckMode.MANUAL_IMMEDIATE
         factory.containerProperties.authExceptionRetryInterval = Duration.ofSeconds(2)
-        factory.consumerFactory = DefaultKafkaConsumerFactory(properties.buildConsumerProperties())
+        factory.consumerFactory = DefaultKafkaConsumerFactory(consumerProperties)
         factory.setCommonErrorHandler(kafkaErrorHandler)
         return factory
     }
