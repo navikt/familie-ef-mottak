c79ac7812fdd81c40c04734398454ffdd3cd70ac
Sandra Lekve <sandra.lekve@icloud.com>
2024-10-31 14:32:41 +0100

    add unprotected

--- src/main/kotlin/no/nav/familie/ef/mottak/api/SøknadskvitteringController.kt
+++ src/main/kotlin/no/nav/familie/ef/mottak/api/SøknadskvitteringController.kt
@@ -2,6 +2,7 @@ package no.nav.familie.ef.mottak.api
 
 import no.nav.familie.ef.mottak.repository.domain.Søknad
 import no.nav.familie.ef.mottak.service.SøknadskvitteringService
+import no.nav.security.token.support.core.api.Unprotected
 import org.springframework.context.annotation.Profile
 import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import org.springframework.web.bind.annotation.GetMapping
@@ -12,9 +13,11 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @Profile("!prod")
 @RequestMapping("api/soknadskvittering", produces = [APPLICATION_JSON_VALUE])
+@Unprotected
 class SøknadskvitteringController(
     val søknadskvitteringService: SøknadskvitteringService,
 ) {
+    @Unprotected
     @GetMapping("{id}")
     fun søknad(
         @PathVariable id: String,
