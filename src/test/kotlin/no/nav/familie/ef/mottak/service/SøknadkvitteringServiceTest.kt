48411900eaf3a34ad0b1ef99faee22054b3d6a25
Fredrik Garseg Mørk <fredrik.1997@hotmail.com>
2024-11-04 15:37:18 +0100

    Lagt til en test for søknadskvitteringservice

---
+++ src/test/kotlin/no/nav/familie/ef/mottak/service/SøknadkvitteringServiceTest.kt
@@ -0,0 +1,35 @@
+package no.nav.familie.ef.mottak.no.nav.familie.ef.mottak.service
+
+import io.mockk.every
+import io.mockk.mockk
+import no.nav.familie.ef.mottak.encryption.EncryptedString
+import no.nav.familie.ef.mottak.no.nav.familie.ef.mottak.util.søknad
+import no.nav.familie.ef.mottak.repository.SøknadRepository
+import no.nav.familie.ef.mottak.repository.VedleggRepository
+import no.nav.familie.ef.mottak.repository.util.findByIdOrThrow
+import no.nav.familie.ef.mottak.service.SøknadskvitteringService
+import no.nav.familie.ef.mottak.service.Testdata
+import no.nav.familie.kontrakter.felles.objectMapper
+import org.junit.Before
+import org.junit.Test
+import kotlin.test.assertTrue
+
+class SøknadkvitteringServiceTest {
+    private val søknadRepository = mockk<SøknadRepository>()
+    private val vedleggRepository = mockk<VedleggRepository>()
+    private val søknadkvitteringService = SøknadskvitteringService(søknadRepository, vedleggRepository)
+
+    @Before
+    fun setup(){
+        every { søknadRepository.findByIdOrThrow("1") } returns søknad(
+            søknadJsonString = EncryptedString(objectMapper.writeValueAsString(Testdata.søknadOvergangsstønad)),
+        )
+        every { vedleggRepository.finnTitlerForSøknadId("1") } returns listOf("")
+    }
+
+    @Test
+    fun `hentSøknadOgMapTilGenereltFormat`(){
+        val resultat = søknadkvitteringService.hentSøknadOgMapTilGenereltFormat("1")
+        assertTrue(resultat.values.firstOrNull() == "Søknad om overgangsstønad (NAV 15-00.01)")
+    }
+}
