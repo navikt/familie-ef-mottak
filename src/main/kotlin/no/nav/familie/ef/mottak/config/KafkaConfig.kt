feda1fca28e7cdb1be36095691f94aef43584f7a
Ole Christian Kvernberg <42737566+olekvernberg@users.noreply.github.com>
2024-11-11 16:53:26 +0100

    Oppdaterer kafka config (#1168)
    * Fikset deprecated kode i kafka config. Setter all config i kafkalistenerfactory og fjerner alt i application.yml for å ha all kafka config på ett sted
    
    * Add deserializers in config

--- src/main/kotlin/no/nav/familie/ef/mottak/config/KafkaConfig.kt
+++ src/main/kotlin/no/nav/familie/ef/mottak/config/KafkaConfig.kt
@@ -1,8 +1,14 @@
 package no.nav.familie.ef.mottak.config
 
+import io.confluent.kafka.serializers.KafkaAvroDeserializer
+import io.confluent.kafka.serializers.KafkaAvroDeserializerConfig
 import no.nav.familie.kafka.KafkaErrorHandler
 import no.nav.joarkjournalfoeringhendelser.JournalfoeringHendelseRecord
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.springframework.beans.factory.ObjectProvider
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties
+import org.springframework.boot.ssl.SslBundles
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.annotation.EnableKafka
@@ -18,12 +24,20 @@ class KafkaConfig {
     fun kafkaJournalføringHendelseListenerContainerFactory(
         properties: KafkaProperties,
         kafkaErrorHandler: KafkaErrorHandler,
+        sslBundles: ObjectProvider<SslBundles>,
     ): ConcurrentKafkaListenerContainerFactory<Long, JournalfoeringHendelseRecord> {
-        properties.properties["specific.avro.reader"] = "true"
+        val consumerProperties = properties.buildConsumerProperties(sslBundles.getIfAvailable())
+        consumerProperties[ConsumerConfig.MAX_POLL_RECORDS_CONFIG] = 1
+        consumerProperties[ConsumerConfig.AUTO_OFFSET_RESET_CONFIG] = "earliest"
+        consumerProperties[ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG] = false
+        consumerProperties[KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG] = true
+        consumerProperties[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
+        consumerProperties[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = KafkaAvroDeserializer::class.java
+
         val factory = ConcurrentKafkaListenerContainerFactory<Long, JournalfoeringHendelseRecord>()
         factory.containerProperties.ackMode = ContainerProperties.AckMode.MANUAL_IMMEDIATE
         factory.containerProperties.authExceptionRetryInterval = Duration.ofSeconds(2)
-        factory.consumerFactory = DefaultKafkaConsumerFactory(properties.buildConsumerProperties())
+        factory.consumerFactory = DefaultKafkaConsumerFactory(consumerProperties)
         factory.setCommonErrorHandler(kafkaErrorHandler)
         return factory
     }
