ece6a62d5381592949cc9584caa94d5489f99ee9
Viktor Solberg <viktorgsolberg@gmail.com>
2024-11-22 14:43:54 +0100

    skal kunne gjøre requests mot familie-pdf i preprod

--- src/main/kotlin/no/nav/familie/ef/mottak/integration/PdfKvitteringClient.kt
+++ src/main/kotlin/no/nav/familie/ef/mottak/integration/PdfKvitteringClient.kt
@@ -1,19 +1,31 @@
 package no.nav.familie.ef.mottak.integration
 
 import no.nav.familie.ef.mottak.util.medContentTypeJsonUTF8
-import no.nav.familie.http.client.AbstractRestClient
+import no.nav.familie.http.client.AbstractPingableRestClient
 import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpHeaders
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestOperations
-import org.springframework.web.util.DefaultUriBuilderFactory
+import org.springframework.web.util.UriComponentsBuilder
+import java.net.URI
 
 @Service
 class PdfKvitteringClient(
-    @Qualifier("restTemplateUnsecured") operations: RestOperations,
-) : AbstractRestClient(operations, "pdf") {
+    @Value("\${familie.pdf.url}")
+    private val uri: URI,
+    @Qualifier("restTemplateAzure")
+    restOperations: RestOperations,
+) : AbstractPingableRestClient(restOperations, "familie-pdf") {
+    override val pingUri: URI =
+        UriComponentsBuilder
+            .fromUri(uri)
+            .pathSegment("api/ping")
+            .build()
+            .toUri()
+
     fun opprettPdf(feltMap: Map<String, Any>): ByteArray {
-        val sendInnUri = DefaultUriBuilderFactory().uriString("https://familie-pdf.intern.dev.nav.no/api").path("pdf/opprett-pdf").build()
-        return postForEntity(sendInnUri, feltMap, HttpHeaders().medContentTypeJsonUTF8())
+        val uri = UriComponentsBuilder.fromUri(uri).pathSegment("api/pdf/opprett-pdf").build().toUri()
+        return postForEntity(uri, feltMap, HttpHeaders().medContentTypeJsonUTF8())
     }
 }
