56ea912b3e9897036492d8d20fa1785bbdb32043
Marcus Schrøder <42798443+marcuasc@users.noreply.github.com>
2025-01-23 12:22:14 +0100

    Legge til skjemanummer som type i feltmap (#1220)
    * Legge til underoverskrift som type i feltmap, funksjon for å lage tekst til søknadstype i underoverksift og tester
    
    * rename til brevkode
    
    * bytte navn fra brevkode til skjemanummer
    
    * ta vekk skjemanummer fra overskriften

--- src/main/kotlin/no/nav/familie/ef/mottak/service/SøknadTilFeltMap.kt
+++ src/main/kotlin/no/nav/familie/ef/mottak/service/SøknadTilFeltMap.kt
@@ -49,7 +49,13 @@ object SøknadTilFeltMap {
     ): FeltMap {
         val finnFelter = finnFelter(søknad)
         val vedlegg = mapTilVedlegg(vedleggTitler)
-        return FeltMap("Søknad om overgangsstønad (NAV 15-00.01)", finnFelter + vedlegg, PdfConfig(true, søknad.innsendingsdetaljer.verdi.språk ?: "nb"))
+        val språk = søknad.innsendingsdetaljer.verdi.språk ?: "nb"
+        return FeltMap(
+            "Søknad om overgangsstønad",
+            finnFelter + vedlegg,
+            PdfConfig(true, språk),
+            getSkjemanummerTekst("overgangsstønad", språk),
+        )
     }
 
     fun mapBarnetilsyn(
@@ -58,7 +64,16 @@ object SøknadTilFeltMap {
     ): FeltMap {
         val finnFelter = finnFelter(søknad)
         val vedlegg = mapTilVedlegg(vedleggTitler)
-        return FeltMap("Søknad om stønad til barnetilsyn (NAV 15-00.02)", finnFelter + vedlegg, PdfConfig(true, søknad.innsendingsdetaljer.verdi.språk ?: "nb"))
+        val språk = søknad.innsendingsdetaljer.verdi.språk ?: "nb"
+        return FeltMap(
+            "Søknad om stønad til barnetilsyn",
+            finnFelter + vedlegg,
+            PdfConfig(
+                true,
+                språk,
+            ),
+            getSkjemanummerTekst("barnetilsyn", språk),
+        )
     }
 
     fun mapSkolepenger(
@@ -67,12 +82,24 @@ object SøknadTilFeltMap {
     ): FeltMap {
         val finnFelter = finnFelter(søknad)
         val vedlegg = mapTilVedlegg(vedleggTitler)
-        return FeltMap("Søknad om stønad til skolepenger (NAV 15-00.04)", finnFelter + vedlegg, PdfConfig(true, søknad.innsendingsdetaljer.verdi.språk ?: "nb"))
+        val språk = søknad.innsendingsdetaljer.verdi.språk ?: "nb"
+        return FeltMap(
+            "Søknad om stønad til skolepenger",
+            finnFelter + vedlegg,
+            PdfConfig(true, språk),
+            getSkjemanummerTekst("skolepenger", språk),
+        )
     }
 
     fun mapSkjemafelter(skjema: SkjemaForArbeidssøker): FeltMap {
         val finnFelter = finnFelter(skjema)
-        return FeltMap("Skjema for arbeidssøker - 15-08.01", finnFelter, PdfConfig(false, skjema.innsendingsdetaljer.verdi.språk ?: "nb"))
+        val språk = skjema.innsendingsdetaljer.verdi.språk ?: "nb"
+        return FeltMap(
+            "Skjema for arbeidssøker",
+            finnFelter,
+            PdfConfig(false, språk),
+            getSkjemanummerTekst("arbeidssøker", språk),
+        )
     }
 
     fun mapEttersending(
@@ -214,4 +241,18 @@ enum class VisningsVariant {
     TABELL,
     VEDLEGG,
     PUNKTLISTE,
+}
+
+private fun getSkjemanummerTekst(
+    skjemanummer: String,
+    språk: String,
+): String? {
+    val skjemanumre =
+        mapOf(
+            "overgangsstønad" to mapOf("nb" to "Skjemanummer: NAV 15-00.01", "en" to "Application number: NAV 15-00.01"),
+            "barnetilsyn" to mapOf("nb" to "Skjemanummer: NAV 15-00.02", "en" to "Application number: NAV 15-00.02"),
+            "skolepenger" to mapOf("nb" to "Skjemanummer: NAV 15-00.04", "en" to "Application number: NAV 15-00.04"),
+            "arbeidssøker" to mapOf("nb" to "Skjemanummer: NAV 15-08.01", "en" to "Application number: NAV 15-08.01"),
+        )
+    return skjemanumre[skjemanummer]?.get(språk)
 }
