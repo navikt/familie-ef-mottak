18e8715ba1427c7ef1c1e643269734defe816294
endrehm <endre.heien.mo@nav.no>
2025-03-24 13:38:48 +0100

    Fjernet unødvendig controller lagt til tidligere og byenviormentstrategy

--- src/main/kotlin/no/nav/familie/ef/mottak/featuretoggle/FeatureToggleService.kt
+++ src/main/kotlin/no/nav/familie/ef/mottak/featuretoggle/FeatureToggleService.kt
@@ -15,10 +15,4 @@ enum class Toggle(
 ) {
     ENDRE_TEST("Endre-test-toggle"),
     ;
-
-    companion object {
-        private val toggles: Map<String, Toggle> = values().associateBy { it.name }
-
-        fun byToggleId(toggleId: String): Toggle = toggles[toggleId] ?: error("Finner ikke toggle for $toggleId")
-    }
 }
