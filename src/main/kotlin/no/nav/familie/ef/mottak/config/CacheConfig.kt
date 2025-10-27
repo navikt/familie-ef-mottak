e90c62a737e42ffe9ff57bfbff01b0496044230e
Ole Christian Kvernberg <42737566+olekvernberg@users.noreply.github.com>
2025-10-27 13:54:33 +0100

    Bruk Spring sin CaffeineCache for korrekt null-håndtering (#1323)

--- src/main/kotlin/no/nav/familie/ef/mottak/config/CacheConfig.kt
+++ src/main/kotlin/no/nav/familie/ef/mottak/config/CacheConfig.kt
@@ -1,11 +1,9 @@
 package no.nav.familie.ef.mottak.config
 
 import com.github.benmanes.caffeine.cache.Caffeine
-import org.springframework.cache.Cache
 import org.springframework.cache.CacheManager
 import org.springframework.cache.annotation.EnableCaching
-import org.springframework.cache.concurrent.ConcurrentMapCache
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager
+import org.springframework.cache.caffeine.CaffeineCacheManager
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
@@ -17,18 +15,15 @@ class CacheConfig {
     @Bean
     @Primary
     fun cacheManager(): CacheManager =
-        object : ConcurrentMapCacheManager() {
-            override fun createConcurrentMapCache(name: String): Cache {
-                val concurrentMap =
-                    Caffeine
-                        .newBuilder()
-                        .maximumSize(1000)
-                        .expireAfterWrite(60, TimeUnit.MINUTES)
-                        .recordStats()
-                        .build<Any, Any>()
-                        .asMap()
-                return ConcurrentMapCache(name, concurrentMap, true)
-            }
+        CaffeineCacheManager().apply {
+            isAllowNullValues = true
+
+            setCaffeine(
+                Caffeine.newBuilder()
+                    .maximumSize(1_000)
+                    .expireAfterWrite(60, TimeUnit.MINUTES)
+                    .recordStats()
+            )
         }
 }
 
